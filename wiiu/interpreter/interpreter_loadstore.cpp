bcd6ad5497e7619e411d65a9c3f6527f997d7d1b
James Benton <james.benton2@gmail.com>
2015-05-27 21:39:31 +0100

    Implement float load/store instructions.

--- wiiu/interpreter/interpreter_loadstore.cpp
+++ wiiu/interpreter/interpreter_loadstore.cpp
@@ -10,18 +10,21 @@ enum LoadFlags
    LoadSignExtend    = 1 << 2, // Sign extend
    LoadByteReverse   = 1 << 3, // Swap bytes
    LoadReserve       = 1 << 4, // lwarx harware reserve
+   LoadZeroRA        = 1 << 5, // Use 0 instead of r0
+   LoadPairedSingles = 1 << 6, // Set fpr.paired1 = d & fpr.paired2 = d
 };
 
 template<typename Type, unsigned flags = 0>
 static void
 loadGeneric(ThreadState *state, Instruction instr)
 {
-   uint32_t d, ea;
+   uint32_t ea;
+   Type d;
 
-   if (instr.rA) {
-      ea = state->gpr[instr.rA];
-   } else {
+   if ((flags & LoadZeroRA) && instr.rA == 0) {
       ea = 0;
+   } else {
+      ea = state->gpr[instr.rA];
    }
 
    if (flags & LoadIndexed) {
@@ -37,16 +40,25 @@ loadGeneric(ThreadState *state, Instruction instr)
       d = gMemory.read<Type>(ea);
    }
   
-   if (flags & LoadSignExtend) {
-      d = sign_extend<bit_width<Type>::value, Type>(d);
+   if (std::is_floating_point<Type>::value) {
+      if (flags & LoadPairedSingles) {
+         state->fpr[instr.rD].value = static_cast<double>(d);
+      } else {
+         state->fpr[instr.rD].paired1 = static_cast<float>(d);
+         state->fpr[instr.rD].paired2 = static_cast<float>(d);
+      }
+   } else {
+      if (flags & LoadSignExtend) {
+         state->gpr[instr.rD] = static_cast<uint32_t>(sign_extend<bit_width<Type>::value, uint64_t>(static_cast<uint64_t>(d)));
+      } else {
+         state->gpr[instr.rD] = static_cast<uint32_t>(d);
+      }
    }
 
    if (flags & LoadReserve) {
       state->reserve = true;
       state->reserveAddress = ea;
    }
-
-   state->gpr[instr.rD] = d;
 
    if (flags & LoadUpdate) {
       state->gpr[instr.rA] = ea;
@@ -56,7 +68,7 @@ loadGeneric(ThreadState *state, Instruction instr)
 static void
 lbz(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint8_t>(state, instr);
+   return loadGeneric<uint8_t, LoadZeroRA>(state, instr);
 }
 
 static void
@@ -74,13 +86,13 @@ lbzux(ThreadState *state, Instruction instr)
 static void
 lbzx(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint8_t, LoadIndexed>(state, instr);
+   return loadGeneric<uint8_t, LoadIndexed | LoadZeroRA>(state, instr);
 }
 
 static void
 lha(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint16_t, LoadSignExtend>(state, instr);
+   return loadGeneric<uint16_t, LoadSignExtend | LoadZeroRA>(state, instr);
 }
 
 static void
@@ -98,19 +110,19 @@ lhaux(ThreadState *state, Instruction instr)
 static void
 lhax(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint16_t, LoadSignExtend | LoadIndexed>(state, instr);
+   return loadGeneric<uint16_t, LoadSignExtend | LoadIndexed | LoadZeroRA>(state, instr);
 }
 
 static void
 lhbrx(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint16_t, LoadByteReverse | LoadIndexed>(state, instr);
+   return loadGeneric<uint16_t, LoadByteReverse | LoadIndexed | LoadZeroRA>(state, instr);
 }
 
 static void
 lhz(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint16_t>(state, instr);
+   return loadGeneric<uint16_t, LoadZeroRA>(state, instr);
 }
 
 static void
@@ -128,25 +140,25 @@ lhzux(ThreadState *state, Instruction instr)
 static void
 lhzx(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint16_t, LoadIndexed>(state, instr);
+   return loadGeneric<uint16_t, LoadIndexed | LoadZeroRA>(state, instr);
 }
 
 static void
 lwbrx(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint32_t, LoadByteReverse | LoadIndexed>(state, instr);
+   return loadGeneric<uint32_t, LoadByteReverse | LoadIndexed | LoadZeroRA>(state, instr);
 }
 
 static void
 lwarx(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint32_t, LoadReserve | LoadIndexed>(state, instr);
+   return loadGeneric<uint32_t, LoadReserve | LoadIndexed | LoadZeroRA>(state, instr);
 }
 
 static void
 lwz(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint32_t>(state, instr);
+   return loadGeneric<uint32_t, LoadZeroRA>(state, instr);
 }
 
 static void
@@ -164,7 +176,55 @@ lwzux(ThreadState *state, Instruction instr)
 static void
 lwzx(ThreadState *state, Instruction instr)
 {
-   return loadGeneric<uint32_t, LoadIndexed>(state, instr);
+   return loadGeneric<uint32_t, LoadIndexed | LoadZeroRA>(state, instr);
+}
+
+static void
+lfs(ThreadState *state, Instruction instr)
+{
+   return loadGeneric<float, LoadPairedSingles | LoadZeroRA>(state, instr);
+}
+
+static void
+lfsu(ThreadState *state, Instruction instr)
+{
+   return loadGeneric<float, LoadPairedSingles | LoadUpdate>(state, instr);
+}
+
+static void
+lfsux(ThreadState *state, Instruction instr)
+{
+   return loadGeneric<float, LoadPairedSingles | LoadUpdate | LoadIndexed>(state, instr);
+}
+
+static void
+lfsx(ThreadState *state, Instruction instr)
+{
+   return loadGeneric<float, LoadPairedSingles | LoadZeroRA | LoadIndexed>(state, instr);
+}
+
+static void
+lfd(ThreadState *state, Instruction instr)
+{
+   return loadGeneric<double, LoadZeroRA>(state, instr);
+}
+
+static void
+lfdu(ThreadState *state, Instruction instr)
+{
+   return loadGeneric<double, LoadUpdate>(state, instr);
+}
+
+static void
+lfdux(ThreadState *state, Instruction instr)
+{
+   return loadGeneric<double, LoadUpdate | LoadIndexed>(state, instr);
+}
+
+static void
+lfdx(ThreadState *state, Instruction instr)
+{
+   return loadGeneric<double, LoadZeroRA | LoadIndexed>(state, instr);
 }
 
 // Load Multiple Words
@@ -244,6 +304,7 @@ enum StoreFlags
    StoreIndexed      = 1 << 1, // Use rB instead of d
    StoreByteReverse  = 1 << 2, // Swap Bytes
    StoreConditional  = 1 << 3, // lward/stwcx Conditional
+   StoreZeroRA       = 1 << 4, // Use 0 instead of r0
 };
 
 template<typename Type, unsigned flags = 0>
@@ -253,12 +314,12 @@ storeGeneric(ThreadState *state, Instruction instr)
    uint32_t ea;
    Type s;
 
-   if (instr.rA) {
-      ea = state->gpr[instr.rA];
-   } else {
+   if ((flags & StoreZeroRA) && instr.rA == 0) {
       ea = 0;
+   } else {
+      ea = state->gpr[instr.rA];
    }
-
+   
    if (flags & StoreIndexed) {
       ea += state->gpr[instr.rB];
    } else {
@@ -277,7 +338,11 @@ storeGeneric(ThreadState *state, Instruction instr)
       }
    }
 
-   s = static_cast<Type>(state->gpr[instr.rS]);
+   if (std::is_floating_point<Type>::value) {
+      s = static_cast<Type>(state->fpr[instr.rS].value);
+   } else {
+      s = static_cast<Type>(state->gpr[instr.rS]);
+   }
 
    if (flags & StoreByteReverse) {
       // Write already does byte_swap, so we writeNoSwap for byte reverse
@@ -294,7 +359,7 @@ storeGeneric(ThreadState *state, Instruction instr)
 static void
 stb(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint8_t>(state, instr);
+   storeGeneric<uint8_t, StoreZeroRA>(state, instr);
 }
 
 static void
@@ -312,13 +377,13 @@ stbux(ThreadState *state, Instruction instr)
 static void
 stbx(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint8_t, StoreIndexed>(state, instr);
+   storeGeneric<uint8_t, StoreZeroRA | StoreIndexed>(state, instr);
 }
 
 static void
 sth(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint16_t>(state, instr);
+   storeGeneric<uint16_t, StoreZeroRA>(state, instr);
 }
 
 static void
@@ -336,13 +401,13 @@ sthux(ThreadState *state, Instruction instr)
 static void
 sthx(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint16_t, StoreIndexed>(state, instr);
+   storeGeneric<uint16_t, StoreZeroRA | StoreIndexed>(state, instr);
 }
 
 static void
 stw(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint32_t>(state, instr);
+   storeGeneric<uint32_t, StoreZeroRA>(state, instr);
 }
 
 static void
@@ -360,25 +425,73 @@ stwux(ThreadState *state, Instruction instr)
 static void
 stwx(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint32_t, StoreIndexed>(state, instr);
+   storeGeneric<uint32_t, StoreZeroRA | StoreIndexed>(state, instr);
 }
 
 static void
 sthbrx(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint16_t, StoreByteReverse | StoreIndexed>(state, instr);
+   storeGeneric<uint16_t, StoreZeroRA | StoreByteReverse | StoreIndexed>(state, instr);
 }
 
 static void
 stwbrx(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint32_t, StoreByteReverse | StoreIndexed>(state, instr);
+   storeGeneric<uint32_t, StoreZeroRA | StoreByteReverse | StoreIndexed>(state, instr);
 }
 
 static void
 stwcx(ThreadState *state, Instruction instr)
 {
-   storeGeneric<uint32_t, StoreConditional| StoreIndexed>(state, instr);
+   storeGeneric<uint32_t, StoreZeroRA | StoreConditional | StoreIndexed>(state, instr);
+}
+
+static void
+stfs(ThreadState *state, Instruction instr)
+{
+   storeGeneric<float, StoreZeroRA>(state, instr);
+}
+
+static void
+stfsu(ThreadState *state, Instruction instr)
+{
+   storeGeneric<float, StoreUpdate>(state, instr);
+}
+
+static void
+stfsux(ThreadState *state, Instruction instr)
+{
+   storeGeneric<float, StoreUpdate | StoreIndexed>(state, instr);
+}
+
+static void
+stfsx(ThreadState *state, Instruction instr)
+{
+   storeGeneric<float, StoreZeroRA | StoreIndexed>(state, instr);
+}
+
+static void
+stfd(ThreadState *state, Instruction instr)
+{
+   storeGeneric<double, StoreZeroRA>(state, instr);
+}
+
+static void
+stfdu(ThreadState *state, Instruction instr)
+{
+   storeGeneric<double, StoreUpdate>(state, instr);
+}
+
+static void
+stfdux(ThreadState *state, Instruction instr)
+{
+   storeGeneric<double, StoreUpdate | StoreIndexed>(state, instr);
+}
+
+static void
+stfdx(ThreadState *state, Instruction instr)
+{
+   storeGeneric<double, StoreZeroRA | StoreIndexed>(state, instr);
 }
 
 // Store Multiple Words
@@ -492,4 +605,20 @@ void Interpreter::registerLoadStoreInstructions()
    RegisterInstruction(stswi);
    RegisterInstruction(stswx);
    RegisterInstruction(stwcx);
+   RegisterInstruction(lfs);
+   RegisterInstruction(lfsu);
+   RegisterInstruction(lfsx);
+   RegisterInstruction(lfsux);
+   RegisterInstruction(lfd);
+   RegisterInstruction(lfdu);
+   RegisterInstruction(lfdx);
+   RegisterInstruction(lfdux);
+   RegisterInstruction(stfs);
+   RegisterInstruction(stfsu);
+   RegisterInstruction(stfsx);
+   RegisterInstruction(stfsux);
+   RegisterInstruction(stfd);
+   RegisterInstruction(stfdu);
+   RegisterInstruction(stfdx);
+   RegisterInstruction(stfdux);
 }
