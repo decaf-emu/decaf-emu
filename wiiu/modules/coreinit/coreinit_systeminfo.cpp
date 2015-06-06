79249c19c0ab3cd97872f6f7fb2c905dc04d6783
James Benton <james.benton2@gmail.com>
2015-06-06 18:39:16 +0100

    Fix OSGetSystemInfo

--- wiiu/modules/coreinit/coreinit_systeminfo.cpp
+++ wiiu/modules/coreinit/coreinit_systeminfo.cpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include "coreinit.h"
 #include "coreinit_systeminfo.h"
 #include "memory.h"
@@ -7,12 +8,13 @@ p32<OSSystemInfo> gSystemInfo;
 p32<OSSystemInfo>
 OSGetSystemInfo()
 {
-   return make_p32<OSSystemInfo>(nullptr);
+   return gSystemInfo;
 }
 
 void
 CoreInit::registerSystemInfoFunctions()
 {
    gSystemInfo = make_p32<OSSystemInfo>(gMemory.alloc(MemoryType::SystemData, sizeof(OSSystemInfo)));
+   gSystemInfo->clockSpeed = CLOCKS_PER_SEC * 4;
    RegisterSystemFunction(OSGetSystemInfo);
 }
