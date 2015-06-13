62149443a0e2cb31fff5463d5ded036d087d7613
James Benton <james.benton2@gmail.com>
2015-06-13 02:33:27 +0100

    Some ghs stubs.

--- wiiu/modules/coreinit/coreinit_ghs.cpp
+++ wiiu/modules/coreinit/coreinit_ghs.cpp
@@ -3,7 +3,10 @@
 #include "coreinit_spinlock.h"
 #include "coreinit_memory.h"
 
+static const auto GHS_FOPEN_MAX = 0x64;
+
 p32<OSSpinLock> ghsSpinLock;
+thread_local uint32_t gErrno = 0;
 
 BOOL
 ghsLock()
@@ -15,6 +18,31 @@ BOOL
 ghsUnlock()
 {
    return OSReleaseSpinLock(ghsSpinLock);
+}
+
+uint32_t
+ghs_flock_ptr()
+{
+   return 0;
+}
+
+uint32_t
+ghs_get_errno()
+{
+   return gErrno;
+}
+
+void
+ghs_set_errno(uint32_t err)
+{
+   gErrno = err;
+}
+
+void
+ghs_flock_file(uint32_t lockIx)
+{
+   assert(lockIx <= GHS_FOPEN_MAX);
+   // Lock mutex for file lockIx
 }
 
 /*
@@ -30,6 +58,10 @@ CoreInit::registerGhsFunctions()
 {
    RegisterSystemFunctionName("__ghsLock", ghsLock);
    RegisterSystemFunctionName("__ghsUnlock", ghsUnlock);
+   RegisterSystemFunctionName("__gh_set_errno", ghs_set_errno);
+   RegisterSystemFunctionName("__gh_get_errno", ghs_get_errno);
+   RegisterSystemFunctionName("__ghs_flock_ptr", ghs_flock_ptr);
+   RegisterSystemFunctionName("__ghs_flock_file", ghs_flock_file);
 }
 
 void
--- wiiu/modules/coreinit/coreinit_ghs.h
+++ wiiu/modules/coreinit/coreinit_ghs.h
@@ -6,3 +6,15 @@ ghsLock();
 
 BOOL
 ghsUnlock();
+
+void
+ghs_set_errno(uint32_t err);
+
+uint32_t
+ghs_get_errno();
+
+uint32_t
+ghs_flock_ptr();
+
+void
+ghs_flock_file(uint32_t lockIx);
