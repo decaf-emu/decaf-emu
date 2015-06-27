b534a3f036a062db9a01968d124c8f3d73a87c04
James Benton <james.benton2@gmail.com>
2015-06-27 15:25:44 +0100

    Added dump of OSGetSystemInfo from real WiiU console.

--- wiiu/modules/coreinit/coreinit_systeminfo.cpp
+++ wiiu/modules/coreinit/coreinit_systeminfo.cpp
@@ -42,4 +42,15 @@ CoreInit::initialiseSystemInformation()
 {
    gSystemInfo = OSAllocFromSystem(sizeof(OSSystemInfo), 4);
    gSystemInfo->clockSpeed = CLOCKS_PER_SEC * 4;
+
+   /*
+   From a WiiU Console:
+   uint32_t 248625000
+   uint32_t 1243125000
+   uint64_t 30373326953884705
+   uint32_t 524288
+   uint32_t 2097152
+   uint32_t 524288
+   uint32_t 1709422149632
+   */
 }
