78924cafe986ca3cf90ea71c9abf98fe85e268f8
Brett Lawson <brett19@gmail.com>
2015-07-02 16:11:48 -0300

    Added more JIT instructions.

--- src/jit/jit_condition.cpp
+++ src/jit/jit_condition.cpp
@@ -90,6 +90,36 @@ cmpli(PPCEmuAssembler& a, Instruction instr)
    return cmpGeneric<uint32_t, CmpImmediate>(a, instr);
 }
 
+// Move from Condition Register
+static bool
+mfcr(PPCEmuAssembler& a, Instruction instr)
+{
+   a.mov(a.eax, a.ppccr);
+   a.mov(a.ppcgpr[instr.rD], a.eax);
+   return true;
+}
+
+// Move to Condition Register Fields
+static bool
+mtcrf(PPCEmuAssembler& a, Instruction instr)
+{
+   uint32_t crm = instr.crm;
+   uint32_t mask = 0;
+   for (auto i = 0u; i < 8; ++i) {
+      if (crm & (1 << i)) {
+         mask |= 0xf << (i * 4);
+      }
+   }
+
+   a.mov(a.eax, a.ppcgpr[instr.rS]);
+   a.and_(a.eax, mask);
+   a.mov(a.ecx, a.ppccr);
+   a.and_(a.ecx, ~mask);
+   a.or_(a.eax, a.ecx);
+   a.mov(a.ppccr, a.eax);
+   return true;
+}
+
 void
 JitManager::registerConditionInstructions()
 {
@@ -97,4 +127,6 @@ JitManager::registerConditionInstructions()
    RegisterInstruction(cmpi);
    RegisterInstruction(cmpl);
    RegisterInstruction(cmpli);
+   RegisterInstruction(mfcr);
+   RegisterInstruction(mtcrf);
 }
