d21a09f3599178b88a2937acbed975ed731df383
James Benton <james.benton2@gmail.com>
2015-07-04 11:46:16 +0100

    Fix formatting in add.s.
    Now uses 3 spaces like source!

--- src/tests/add.s
+++ src/tests/add.s
@@ -1,45 +1,44 @@
 addSimple:
-	# in r1 = 1
-	# in r2 = 2
-	add r3, r1, r2
-	blr
-	# out r3 = 3
+   # in r1 = 1
+   # in r2 = 2
+   add r3, r1, r2
+   blr
+   # out r3 = 3
 
 addSimpleRecordWithSO:
-	# in r1 = 1
-	# in r2 = 2
-  # in xer.so = 1
-	add. r3, r1, r2
-	blr
-	# out r3 = 3
-	# out crf0 = Positive | SummaryOverflow
+   # in r1 = 1
+   # in r2 = 2
+   # in xer.so = 1
+   add. r3, r1, r2
+   blr
+   # out r3 = 3
+   # out crf0 = Positive | SummaryOverflow
 
 addSimpleRecordNoSO:
-	# in r1 = 1
-	# in r2 = 2
-  # in xer.so = 0
-	add. r3, r1, r2
-	blr
-	# out r3 = 3
-	# out crf0 = Positive
-  
+   # in r1 = 1
+   # in r2 = 2
+   # in xer.so = 0
+   add. r3, r1, r2
+   blr
+   # out r3 = 3
+   # out crf0 = Positive
+
 testCRF:
-  # in r1 = 1
-  # in r2 = 2
-  # in r3 = 0
-  # in xer.so = 0
-  mtcr r3
-  add. r3, r1, r2
-  mfcr r4
-  blr
-  # out r3 = 3
-  # out r4 = 0x40000000
-  # out crf0 = Positive
-  # out crf1 = 0
-  # out crf2 = 0
-  # out crf3 = 0
-  # out crf4 = 0
-  # out crf5 = 0
-  # out crf6 = 0
-  # out crf7 = 0
-  
+   # in r1 = 1
+   # in r2 = 2
+   # in r3 = 0
+   # in xer.so = 0
+   mtcr r3
+   add. r3, r1, r2
+   mfcr r4
+   blr
+   # out r3 = 3
+   # out r4 = 0x40000000
+   # out crf0 = Positive
+   # out crf1 = 0
+   # out crf2 = 0
+   # out crf3 = 0
+   # out crf4 = 0
+   # out crf5 = 0
+   # out crf6 = 0
+   # out crf7 = 0
