d6617f463c2856f3950f0f667658cb2f1f9a03fb
James Benton <james.benton2@gmail.com>
2015-07-08 22:16:02 +0100

    Stub some device functions

--- src/modules/coreinit/coreinit_device.cpp
+++ src/modules/coreinit/coreinit_device.cpp
@@ -1,14 +1,40 @@
 #include "coreinit.h"
 #include "coreinit_device.h"
 
+static BOOL
+OSDriver_Register(uint32_t r3, uint32_t r4, void *somePtr, uint32_t someID, uint32_t r7, uint32_t *someOutValue, uint32_t r9)
+{
+   return FALSE;
+}
+
+void
+OSEnforceInorderIO()
+{
+}
+
 uint16_t
 OSReadRegister16(uint32_t device, uint32_t id)
 {
    return 0;
 }
 
+uint32_t
+OSReadRegister32Ex(uint32_t device, uint32_t id)
+{
+   return 0;
+}
+
+void
+OSWriteRegister32Ex(uint32_t device, uint32_t id, uint32_t value)
+{
+}
+
 void
 CoreInit::registerDeviceFunctions()
 {
+   RegisterKernelFunction(OSDriver_Register);
    RegisterKernelFunction(OSReadRegister16);
+   RegisterKernelFunction(OSEnforceInorderIO);
+   RegisterKernelFunctionName("__OSReadRegister32Ex", OSReadRegister32Ex);
+   RegisterKernelFunctionName("__OSWriteRegister32Ex", OSWriteRegister32Ex);
 }
--- src/modules/coreinit/coreinit_device.h
+++ src/modules/coreinit/coreinit_device.h
@@ -3,3 +3,9 @@
 
 uint16_t
 OSReadRegister16(uint32_t device, uint32_t id);
+
+uint32_t
+OSReadRegister32Ex(uint32_t device, uint32_t id);
+
+void
+OSWriteRegister32Ex(uint32_t device, uint32_t id, uint32_t value);
