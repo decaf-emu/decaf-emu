fa876d28fd5fd34c1393bb88189bd13f614845ef
aspico <you@example.com>
2015-07-30 21:59:07 +0100

    changed to hex constants for logical and tests

--- src/tests/and.s
+++ src/tests/and.s
@@ -1,36 +1,36 @@
 andTest:
-    # in r1 = 12312312
+    # in r1 = 0x13AB42FF
     # in r2 = 0
     and r3, r1, r2
     # out r3 = 0
 
 andTest2:
-    # in r1 = 12312312
-    # in r2 = 4294967295
+    # in r1 = 0x13AB42FF
+    # in r2 = 0xFFFFFFFF
     and r3, r1, r2
-    # out r3 = 12312312
+    # out r3 = 0x13AB42FF
 
 andTest3:
-    # in r1 = 12312312
-    # in r2 = 49875685
+    # in r1 = 0x13AB42FF
+    # in r2 = 0x6A40BBF4
     and r3, r1, r2
-    # out r3 = 12126944
+    # out r3 = 0x020002F4
 
 andComplementTest:
-    # in r1 = 12312312
+    # in r1 = 0x13AB42FF
     # in r2 = 0
     andc r3, r1, r2
-    # out r3 = 12312312
+    # out r3 = 0x13AB42FF
 
 andComplementTest2:
-    # in r1 = 12312312
-    # in r2 = 4294967295
+    # in r1 = 0x13AB42FF
+    # in r2 = 0xFFFFFFFF
     andc r3, r1, r2
     # out r3 = 0
 
 andComplementTest3:
-    # in r1 = 12312312
-    # in r2 = 49875685
+    # in r1 = 0x13AB42FF
+    # in r2 = 0x95BF440B
     andc r3, r1, r2
-    # out r3 = 185368
+    # out r3 = 0x020002F4
     
