8b64dddc1fa13af77277a993dab5de6e7c05ae7f
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-08-01 00:20:36 -0700

    coreinit: implement OSTicksToCalendarTime

--- src/modules/coreinit/coreinit_time.cpp
+++ src/modules/coreinit/coreinit_time.cpp
@@ -40,10 +40,26 @@ OSTimeToChrono(OSTime time)
 }
 
 void
+OSTicksToCalendarTime(OSTime time, OSCalendarTime* calendarTime)
+{
+   auto chrono = OSTimeToChrono(time);
+   std::time_t system_time_t = std::chrono::system_clock::to_time_t(chrono);
+   std::tm* tm = std::localtime(&system_time_t);
+
+   calendarTime->tm_sec = tm->tm_sec;
+   calendarTime->tm_min = tm->tm_min;
+   calendarTime->tm_hour = tm->tm_hour;
+   calendarTime->tm_mday = tm->tm_mday;
+   calendarTime->tm_mon = tm->tm_mon;
+   calendarTime->tm_year = tm->tm_year + 1900; // posix tm_year is year - 1900
+}
+
+void
 CoreInit::registerTimeFunctions()
 {
    RegisterKernelFunction(OSGetTime);
    RegisterKernelFunction(OSGetTick);
    RegisterKernelFunction(OSGetSystemTime);
    RegisterKernelFunction(OSGetSystemTick);
+   RegisterKernelFunction(OSTicksToCalendarTime);
 }
