64dd4cb667564c9f54e6ccd7eaf7fe14b4539ada
James Benton <james.benton2@gmail.com>
2015-08-03 00:49:01 +0100

    Stub ProcUIRegisterCallback, ProcUISetMEM1Storage.

--- src/modules/proc_ui/proc_ui_core.cpp
+++ src/modules/proc_ui/proc_ui_core.cpp
@@ -29,13 +29,27 @@ ProcUIInitEx(ProcUISaveCallbackEx saveCallbackEx, void *arg)
 uint32_t
 ProcUIProcessMessages(BOOL unk1)
 {
+   // TODO: ProcUIProcessMessages
    return 0;
 }
 
 uint32_t
 ProcUISubProcessMessages(BOOL unk1)
 {
+   // TODO: ProcUISubProcessMessages
    return 0;
+}
+
+void
+ProcUIRegisterCallback(ProcUICallbackType::Type type, ProcUICallback callback, void *param, uint32_t unk)
+{
+   // TODO: ProcUIRegisterCallback
+}
+
+void
+ProcUISetMEM1Storage(void *buffer, uint32_t size)
+{
+   // TODO: ProcUISetMEM1Storage
 }
 
 void
@@ -45,4 +59,6 @@ ProcUI::registerCoreFunctions()
    RegisterKernelFunction(ProcUIInitEx);
    RegisterKernelFunction(ProcUIProcessMessages);
    RegisterKernelFunction(ProcUISubProcessMessages);
+   RegisterKernelFunction(ProcUIRegisterCallback);
+   RegisterKernelFunction(ProcUISetMEM1Storage);
 }
--- src/modules/proc_ui/proc_ui_core.h
+++ src/modules/proc_ui/proc_ui_core.h
@@ -1,8 +1,17 @@
 #pragma once
 #include "systemtypes.h"
 
+// Unknown parameters.
 using ProcUISaveCallback = wfunc_ptr<void>;
-using ProcUISaveCallbackEx = wfunc_ptr<void>;
+using ProcUISaveCallbackEx = wfunc_ptr<uint32_t, void*>;
+using ProcUICallback = wfunc_ptr<uint32_t, void *>;
+
+namespace ProcUICallbackType
+{
+enum Type
+{
+};
+}
 
 void
 ProcUIInit(ProcUISaveCallback saveCallback);
@@ -15,3 +24,9 @@ ProcUIProcessMessages(BOOL unk1);
 
 uint32_t
 ProcUISubProcessMessages(BOOL unk1);
+
+void
+ProcUIRegisterCallback(ProcUICallbackType::Type type, ProcUICallback callback, void *param, uint32_t unk);
+
+void
+ProcUISetMEM1Storage(void *buffer, uint32_t size);
