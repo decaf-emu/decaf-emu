18e45d71c629fea63061faa844b1f88f5e0b29cf
James Benton <james.benton2@gmail.com>
2015-09-01 23:56:37 +0100

    Implement SAVEGetSharedDataTitlePath.

--- src/modules/nn_save/nn_save_core.cpp
+++ src/modules/nn_save/nn_save_core.cpp
@@ -18,10 +18,25 @@ SAVEInitSaveDir(uint8_t userID)
    return SaveStatus::OK;
 }
 
+SaveStatus
+SAVEGetSharedDataTitlePath(uint64_t titleID, const char *dir, char *buffer, uint32_t bufferSize)
+{
+   auto titleLo = static_cast<uint32_t>(titleID & 0xffffffff);
+   auto titleHi = static_cast<uint32_t>(titleID >> 32);
+   auto result = snprintf(buffer, bufferSize, "/vol/storage_mlc01/sys/title/%08x/%08x/content/%s", titleHi, titleLo, dir);
+
+   if (result < 0 || result >= bufferSize) {
+      return SaveStatus::FatalError;
+   }
+
+   return SaveStatus::OK;
+}
+
 void
 NNSave::registerCoreFunctions()
 {
    RegisterKernelFunction(SAVEInit);
    RegisterKernelFunction(SAVEShutdown);
    RegisterKernelFunction(SAVEInitSaveDir);
+   RegisterKernelFunction(SAVEGetSharedDataTitlePath);
 }
--- src/modules/nn_save/nn_save_core.h
+++ src/modules/nn_save/nn_save_core.h
@@ -12,3 +12,6 @@ SAVEShutdown();
 
 SaveStatus
 SAVEInitSaveDir(uint8_t userID);
+
+SaveStatus
+SAVEGetSharedDataTitlePath(uint64_t titleID, const char *dir, char *buffer, uint32_t bufferSize);
