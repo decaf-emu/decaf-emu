f8b73c9c627699492e96c9ebdb74be889345c3de
James Benton <james.benton2@gmail.com>
2015-09-05 17:37:13 +0100

    Stub KPADGetMplsWorkSize, KPADSetMplsWorkArea

--- src/modules/padscore/padscore_kpad.cpp
+++ src/modules/padscore/padscore_kpad.cpp
@@ -14,9 +14,23 @@ KPADInitEx(uint32_t unk1, uint32_t unk2)
    WPADInit();
 }
 
+uint32_t
+KPADGetMplsWorkSize()
+{
+   return 1024;
+}
+
+void
+KPADSetMplsWorkarea(char *buffer)
+{
+   // sizeof(buffer) = KPADGetMplsWorkSize()
+}
+
 void
 PadScore::registerKPADFunctions()
 {
    RegisterKernelFunction(KPADInit);
    RegisterKernelFunction(KPADInitEx);
+   RegisterKernelFunction(KPADGetMplsWorkSize);
+   RegisterKernelFunction(KPADSetMplsWorkarea);
 }
--- src/modules/padscore/padscore_kpad.h
+++ src/modules/padscore/padscore_kpad.h
@@ -6,3 +6,9 @@ KPADInit();
 
 void
 KPADInitEx(uint32_t unk1, uint32_t unk2);
+
+uint32_t
+KPADGetMplsWorkSize();
+
+void
+KPADSetMplsWorkarea(char *buffer);
