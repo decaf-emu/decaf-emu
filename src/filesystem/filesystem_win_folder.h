3dcb6e3c477bb4acdc6a5b84ce5f96118e081898
James Benton <james.benton2@gmail.com>
2015-09-05 17:22:10 +0100

    Fix FSReadDir recursion for skipping . and ..

--- src/filesystem/filesystem_win_folder.h
+++ src/filesystem/filesystem_win_folder.h
@@ -34,11 +34,6 @@ struct HostFolder : public Folder
          return false;
       }
 
-      if (strcmp(findData.cFileName, ".") == 0 || strcmp(findData.cFileName, "..") == 0) {
-         // Skip over . and .. entries
-         return read(entry);
-      }
-
       entry.name = findData.cFileName;
 
       if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
@@ -51,6 +46,11 @@ struct HostFolder : public Folder
 
       if (!FindNextFileA(findHandle, &findData)) {
          close();
+      }
+
+      if (strcmp(findData.cFileName, ".") == 0 || strcmp(findData.cFileName, "..") == 0) {
+         // Skip over . and .. entries
+         return read(entry);
       }
 
       return true;
