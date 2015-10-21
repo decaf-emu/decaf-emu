5eaf5592f54bb2c9a84a17ba240c7c706503c101
Brett Lawson <brett19@gmail.com>
2015-10-21 02:04:25 -0300

    Fix building of GX2_NULL.

--- src/modules/gx2/null/gx2null_draw.cpp
+++ src/modules/gx2/null/gx2null_draw.cpp
@@ -21,6 +21,20 @@ GX2ClearBuffersEx(GX2ColorBuffer *colorBuffer,
 }
 
 void
+GX2ClearColor(GX2ColorBuffer *colorBuffer,
+   float red, float green, float blue, float alpha)
+{
+}
+
+void
+GX2ClearDepthStencilEx(GX2DepthBuffer *depthBuffer,
+   float depth,
+   uint8_t stencil,
+   GX2ClearFlags::Flags unk2)
+{
+}
+
+void
 GX2SetAttribBuffer(uint32_t unk1,
                    uint32_t unk2,
                    uint32_t unk3,
@@ -33,6 +47,16 @@ GX2DrawEx(GX2PrimitiveMode::Mode mode,
           uint32_t unk1,
           uint32_t unk2,
           uint32_t unk3)
+{
+}
+
+void
+GX2DrawIndexedEx(GX2PrimitiveMode::Mode mode,
+   uint32_t numVertices,
+   GX2IndexType::Type indexType,
+   void *indices,
+   uint32_t offset,
+   uint32_t numInstances)
 {
 }
 
