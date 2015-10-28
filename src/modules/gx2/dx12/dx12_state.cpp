aeef989e1ef54dddca660e9dc728c42e05f818a2
Brett Lawson <brett19@gmail.com>
2015-10-28 04:08:32 -0300

    Samplers...  Arg.

--- src/modules/gx2/dx12/dx12_state.cpp
+++ src/modules/gx2/dx12/dx12_state.cpp
@@ -200,9 +200,9 @@ void dx::initialise()
       for (int i = 0; i < GX2_NUM_SAMPLERS; ++i) {
          auto &sampler = samplers[i] = {};
          sampler.Filter = D3D12_FILTER_MIN_MAG_MIP_POINT;
-         sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_BORDER;
-         sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_BORDER;
-         sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_BORDER;
+         sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+         sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+         sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
          sampler.MipLODBias = 0;
          sampler.MaxAnisotropy = 0;
          sampler.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
