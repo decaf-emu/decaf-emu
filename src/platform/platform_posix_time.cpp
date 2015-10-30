aba3792cf8d45b37236955db0b5444e16352b909
archshift <gh@archshift.com>
2015-10-29 23:37:47 -0700

    platform_posix_time.cpp: place code in proper namespace

--- src/platform/platform_posix_time.cpp
+++ src/platform/platform_posix_time.cpp
@@ -2,11 +2,16 @@
 
 #ifdef PLATFORM_POSIX
 
+namespace platform
+{
+
 tm localtime(const std::time_t& time)
 {
    std::tm tm_snapshot;
    localtime_r(&time, &tm_snapshot);
    return tm_snapshot;
+}
+
 }
 
 #endif
