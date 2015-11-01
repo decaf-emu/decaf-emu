1119f18015d78da17649db60ff6048eab012b8f1
Brett Lawson <brett19@gmail.com>
2015-11-01 13:36:24 -0400

    Stack alignment is a pain in the butt.

--- src/cpu/jit/jit.cpp
+++ src/cpu/jit/jit.cpp
@@ -34,14 +34,14 @@ namespace jit
       a.push(a.zdi);
       a.push(a.zsi);
       a.push(asmjit::x86::r10);
-      a.sub(a.zsp, 0x2c);
+      a.sub(a.zsp, 0x38);
       a.mov(a.zbx, a.zcx);
       a.mov(asmjit::x86::r10, a.zdx);
       a.mov(a.zsi, static_cast<uint64_t>(mem::base()));
       a.jmp(asmjit::x86::r8d);
 
       a.bind(extroLabel);
-      a.add(a.zsp, 0x2c);
+      a.add(a.zsp, 0x38);
       a.pop(asmjit::x86::r10);
       a.pop(a.zsi);
       a.pop(a.zdi);
