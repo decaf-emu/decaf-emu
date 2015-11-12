5c0cc73f23682eb2ef9f12e4c86bb37fb200464e
James Benton <james.benton2@gmail.com>
2015-11-12 22:55:18 +0000

    Implement GX2WaitTimeStamp.
    Should work... But it's not the best implementation,
    not sure if there is a better way yet.

--- src/modules/gx2/gx2_event.cpp
+++ src/modules/gx2/gx2_event.cpp
@@ -2,20 +2,23 @@
 #include "gx2_event.h"
 #include "modules/coreinit/coreinit_alarm.h"
 #include "modules/coreinit/coreinit_memheap.h"
+#include "modules/coreinit/coreinit_mutex.h"
+#include "modules/coreinit/coreinit_scheduler.h"
 #include "modules/coreinit/coreinit_systeminfo.h"
 #include "modules/coreinit/coreinit_thread.h"
 #include "modules/coreinit/coreinit_threadqueue.h"
 #include "modules/coreinit/coreinit_time.h"
 #include "utils/wfunc_call.h"
+#include <atomic>
 #include <chrono>
 
-static OSTime
+static std::atomic_int64_t
 gLastVsync = 0;
 
-static OSTime
+static std::atomic_int64_t
 gLastSubmittedTimestamp = 0;
 
-static OSTime
+static std::atomic_int64_t
 gRetiredTimestamp = 0;
 
 static virtual_ptr<OSThreadQueue>
@@ -26,6 +29,9 @@ gVsyncAlarm;
 
 static AlarmCallback
 pVsyncAlarmHandler = nullptr;
+
+static virtual_ptr<OSThreadQueue>
+gWaitTimeStampQueue = nullptr;
 
 struct EventCallbackData
 {
@@ -54,6 +60,7 @@ GX2WaitForVsync()
 void
 GX2WaitForFlip()
 {
+   // TODO: What is a flip? :D
 }
 
 
@@ -83,21 +90,28 @@ GX2GetEventCallback(GX2EventType::Value type, be_GX2EventCallbackFunction *funcO
 OSTime
 GX2GetRetiredTimeStamp()
 {
-   return gRetiredTimestamp;
+   return gRetiredTimestamp.load(std::memory_order_acquire);
 }
 
 
 OSTime
 GX2GetLastSubmittedTimeStamp()
 {
-   return gLastSubmittedTimestamp;
+   return gLastSubmittedTimestamp.load(std::memory_order_acquire);
 }
 
 
 BOOL
 GX2WaitTimeStamp(OSTime time)
 {
-   // TODO: Sleep until GPU has processed TimeStamp
+   OSLockScheduler();
+
+   while (gRetiredTimestamp.load(std::memory_order_acquire) < time) {
+      OSSleepThreadNoLock(gWaitTimeStampQueue);
+      OSRescheduleNoLock();
+   }
+
+   OSUnlockScheduler();
    return TRUE;
 }
 
@@ -108,11 +122,13 @@ namespace gx2
 namespace internal
 {
 
+
 void
 initVsync()
 {
    gVsyncThreadQueue = OSAllocFromSystem<OSThreadQueue>();
    gVsyncAlarm = OSAllocFromSystem<OSAlarm>();
+   gWaitTimeStampQueue = OSAllocFromSystem<OSThreadQueue>();
 
    auto ticks = static_cast<OSTime>(OSGetSystemInfo()->clockSpeed / 4) / 60;
    OSCreateAlarm(gVsyncAlarm);
@@ -123,7 +139,7 @@ initVsync()
 void
 vsyncAlarmHandler(OSAlarm *alarm, OSContext *context)
 {
-   gLastVsync = OSGetSystemTime();
+   gLastVsync.store(OSGetSystemTime(), std::memory_order_release);
    OSWakeupThread(gVsyncThreadQueue);
    auto callback = gEventCallbacks[GX2EventType::Vsync];
 
@@ -136,14 +152,18 @@ vsyncAlarmHandler(OSAlarm *alarm, OSContext *context)
 void
 setRetiredTimestamp(OSTime timestamp)
 {
-   gRetiredTimestamp = timestamp;
+   // TODO: Is there a non-race way to do this without acquiring scheduler lock?
+   OSLockScheduler();
+   gRetiredTimestamp.store(timestamp, std::memory_order_release);
+   OSWakeupThreadNoLock(gWaitTimeStampQueue);
+   OSUnlockScheduler();
 }
 
 
 void
 setLastSubmittedTimestamp(OSTime timestamp)
 {
-   gLastSubmittedTimestamp = timestamp;
+   gLastSubmittedTimestamp.store(timestamp, std::memory_order_release);
 }
 
 } // namespace internal
