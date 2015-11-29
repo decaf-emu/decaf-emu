95bcca1ff34e3cc41930b75ca19ea6c01fef8034
James Benton <james.benton2@gmail.com>
2015-11-29 12:49:05 +0000

    debuglog: Provide debug printf on non-MSVC targets

--- src/utils/debuglog.h
+++ src/utils/debuglog.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <spdlog/spdlog.h>
+#include "platform/platform.h"
 
 template<typename... Args>
 static void
-debugPrint(fmt::CStringRef msg, Args... args) {
+debugPrint(fmt::CStringRef msg, Args... args)
+{
    auto out = fmt::format(msg, args...);
    out += "\n";
+
+#ifdef PLATFORM_WINDOWS
    OutputDebugStringA(out.c_str());
+#else
+   printf("%s", out.c_str());
+#endif
 }
