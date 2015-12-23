86ffc4d3a580c413cfa97383c9821bba368d8d6c
Andrew Church <achurch@users.noreply.github.com>
2015-12-23 14:36:29 +0900

    Use separate TV and DRC windows with SDL. Also listen for SDL window close button events, and save the current window positions in the config when exiting.

--- src/platform/platform_glfw_ui.cpp
+++ src/platform/platform_glfw_ui.cpp
@@ -12,6 +12,18 @@ static const auto TvWidth = 1280.0f;
 static const auto TvHeight = 720.0f;
 static const auto TvScaleFactor = 0.65f;
 
+static int
+getWindowWidth()
+{
+   return std::max(platform::ui::getTvWidth(), platform::ui::getDrcWidth());
+}
+
+static int
+getWindowHeight()
+{
+   return platform::ui::getTvHeight() + platform::ui::getDrcHeight();
+}
+
 namespace platform
 {
 
@@ -38,7 +50,7 @@ init()
 }
 
 bool
-createWindow(const std::string &title)
+createWindows(const std::string &tvTitle, const std::string &drcTitle)
 {
    auto width = getWindowWidth();
    auto height = getWindowHeight();
@@ -66,33 +78,49 @@ shutdown()
 }
 
 void
-swapBuffers()
-{
-   glfwSwapBuffers(glfw::gWindow);
-}
-
-void
 activateContext()
 {
    glfwMakeContextCurrent(glfw::gWindow);
 }
 
 void
-releaseContext()
+swapBuffers()
 {
-   glfwMakeContextCurrent(nullptr);
+   glfwSwapBuffers(glfw::gWindow);
 }
 
-int
-getWindowWidth()
+void
+bindDrcWindow()
 {
-   return std::max(getTvWidth(), getDrcWidth());
+   float wndWidth = static_cast<float>(getWindowWidth());
+   float wndHeight = static_cast<float>(getWindowHeight());
+   float tvWidth = static_cast<float>(getTvWidth());
+   float tvHeight = static_cast<float>(getTvHeight());
+   float drcWidth = static_cast<float>(getDrcWidth());
+   float drcHeight = static_cast<float>(getDrcHeight());
+
+   float tvTop = 0;
+   float tvBottom = tvTop + tvHeight;
+   float drcTop = tvBottom;
+   float drcLeft = (wndWidth - drcWidth) / 2;
+   float drcBottom = drcTop + drcHeight;
+
+   glViewport(drcLeft, wndHeight - drcBottom, drcWidth, drcHeight);
 }
 
-int
-getWindowHeight()
+void
+bindTvWindow()
 {
-   return getTvHeight() + getDrcHeight();
+   float wndWidth = static_cast<float>(getWindowWidth());
+   float wndHeight = static_cast<float>(getWindowHeight());
+   float tvWidth = static_cast<float>(getTvWidth());
+   float tvHeight = static_cast<float>(getTvHeight());
+
+   float tvTop = 0;
+   float tvLeft = (wndWidth - tvWidth) / 2;
+   float tvBottom = tvTop + tvHeight;
+
+   glViewport(tvLeft, wndHeight - tvBottom, tvWidth, tvHeight);
 }
 
 int
--- src/platform/platform_sdl_ui.cpp
+++ src/platform/platform_sdl_ui.cpp
@@ -1,8 +1,19 @@
+#include "config.h"
+#include "platform.h"
 #include "platform_sdl.h"
 #include "platform_ui.h"
 #include "gpu/driver.h"
 #include "utils/log.h"
 #include <SDL.h>
+#include <glbinding/gl/gl.h>
+
+#if defined(PLATFORM_WINDOWS)
+   #include <windows.h>  // For GetSystemMetrics()
+#elif defined(PLATFORM_LINUX)
+   #include <SDL_syswm.h>
+   #include <X11/Xatom.h>
+   #include <X11/Xlib.h>
+#endif
 
 static const auto DrcWidth = 854.0f;
 static const auto DrcHeight = 480.0f;
@@ -12,8 +23,7 @@ static const auto TvWidth = 1280.0f;
 static const auto TvHeight = 720.0f;
 static const auto TvScaleFactor = 0.65f;
 
-static SDL_GLContext tvContext = nullptr;
-static SDL_GLContext drcContext = nullptr;
+static SDL_GLContext glContext = nullptr;
 
 namespace platform
 {
@@ -24,13 +34,71 @@ namespace sdl
 bool shouldQuit = false;
 
 SDL_Window *tvWindow = nullptr;
-// TODO: Separate window not yet implemented.
 SDL_Window *drcWindow = nullptr;
 
 } // namespace sdl
 
 namespace ui
 {
+
+static int
+getWindowBorderHeight(SDL_Window *window)
+{
+   int height = 50;  // A hopefully reasonable default.
+
+#if defined(PLATFORM_WINDOWS)
+   // TODO: Is this correct?
+   height = GetSystemMetrics(SM_CYBORDER) * 2 + GetSystemMetrics(SM_CYCAPTION);
+
+#elif defined(PLATFORM_LINUX)
+   // Linux (more accurately X11) doesn't have any reliable way to get the
+   // border size of a window before it has been shown (or, for that matter,
+   // at any given time _after_ it has been shown).  What we can do is look
+   // through the window list for a window with _NET_FRAME_EXTENTS set, and
+   // assume that the border widths reported there are used for all windows.
+   // Since we only use this for setting initial window positions, it
+   // shouldn't be too bad even if we get the value wrong.
+
+   SDL_SysWMinfo info;
+   SDL_VERSION(&info.version);
+
+   if (window && SDL_GetWindowWMInfo(sdl::tvWindow, &info)) {
+      if (info.subsystem == SDL_SYSWM_X11) {
+         Display *display = info.info.x11.display;
+         Atom atom_NET_FRAME_EXTENTS = XInternAtom(
+            display, "_NET_FRAME_EXTENTS", True);
+         if (atom_NET_FRAME_EXTENTS) {
+            Window *windows;
+            unsigned int numWindows = 0;
+            Window dummyWindow;
+            XQueryTree(display, DefaultRootWindow(display), &dummyWindow,
+                       &dummyWindow, &windows, &numWindows);
+            for (unsigned int i = 0; i < numWindows; i++) {
+               Atom type;
+               int format;
+               unsigned long numItems, bytesAfter;
+               unsigned char *value;
+               int result = XGetWindowProperty(
+                  display, windows[i], atom_NET_FRAME_EXTENTS, 0, 4, False,
+                  AnyPropertyType, &type, &format, &numItems, &bytesAfter,
+                  &value);
+               if (result == Success && format == 32 && numItems == 4) {
+                  auto data = reinterpret_cast<const unsigned long *>(value);
+                  height = data[2] + data[3];
+                  height = std::min(height, 100);  // Sanity check, just in case.
+                  break;
+               }
+            }
+         }
+      } else {
+         gLog->warn("Unhandled window system {}, guessing border height",
+                    info.subsystem);
+      }
+   }
+#endif  // TODO: Need OS X code.
+
+   return height;
+}
 
 bool
 init()
@@ -50,7 +118,7 @@ init()
 }
 
 bool
-createWindow(const std::string &title)
+createWindows(const std::string &tvTitle, const std::string &drcTitle)
 {
    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
@@ -66,23 +134,66 @@ createWindow(const std::string &title)
                        SDL_GL_CONTEXT_PROFILE_CORE);
 #endif
 
-   uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI;
+   const uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI;
+   int tvX, tvY;
+   int drcX, drcY;
+
+   bool positionsLoaded;
+   if (config::ui::tv_window_x != INT_MIN) {
+      tvX = config::ui::tv_window_x;
+      tvY = config::ui::tv_window_y;
+      drcX = config::ui::drc_window_x;
+      drcY = config::ui::drc_window_y;
+      positionsLoaded = true;
+   } else {
+      positionsLoaded = false;
+   }
+
+   // If no position was set in the config, add the DRC window height to the
+   // TV window and then resize after opening the TV window, so the DRC window
+   // is guaranteed to fit on screen.
+   int tvHeight = getTvHeight();
+   if (!positionsLoaded) {
+      tvHeight += getWindowBorderHeight(nullptr) + getDrcHeight();
+      tvX = SDL_WINDOWPOS_UNDEFINED;
+      tvY = SDL_WINDOWPOS_UNDEFINED;
+   }
    sdl::tvWindow = SDL_CreateWindow(
-      title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-      getWindowWidth(), getWindowHeight(), flags);
+      tvTitle.c_str(), tvX, tvY, getTvWidth(), tvHeight, flags);
    if (!sdl::tvWindow) {
       gLog->error("Failed to create TV window: {}", SDL_GetError());
       return false;
    }
 
-   tvContext = SDL_GL_CreateContext(sdl::tvWindow);
-   if (!tvContext) {
-      gLog->error("Failed to create OpenGL context for TV window: {}",
-                  SDL_GetError());
+   if (!positionsLoaded) {
+      SDL_SetWindowSize(sdl::tvWindow, getTvWidth(), getTvHeight());
+      SDL_GetWindowPosition(sdl::tvWindow, &tvX, &tvY);
+      drcX = tvX + (getTvWidth() - getDrcWidth()) / 2;
+      drcY = tvY + getTvHeight() + getWindowBorderHeight(sdl::tvWindow);
+   }
+
+   sdl::drcWindow = SDL_CreateWindow(
+      drcTitle.c_str(), drcX, drcY, getDrcWidth(), getDrcHeight(), flags);
+   if (!sdl::drcWindow) {
+      gLog->error("Failed to create DRC window: {}", SDL_GetError());
       SDL_DestroyWindow(sdl::tvWindow);
       sdl::tvWindow = nullptr;
       return false;
    }
+
+   glContext = SDL_GL_CreateContext(sdl::tvWindow);
+   if (!glContext) {
+      gLog->error("Failed to create OpenGL context: {}", SDL_GetError());
+      SDL_DestroyWindow(sdl::drcWindow);
+      sdl::drcWindow = nullptr;
+      SDL_DestroyWindow(sdl::tvWindow);
+      sdl::tvWindow = nullptr;
+      return false;
+   }
+
+   // SDL won't let the rendering thread take over the context unless we
+   // explicitly release it from this thread.
+   SDL_GL_MakeCurrent(sdl::tvWindow, NULL);
 
    return true;
 }
@@ -103,37 +214,44 @@ run()
 void
 shutdown()
 {
+   if (sdl::tvWindow) {
+      SDL_GetWindowPosition(sdl::tvWindow,
+                            &config::ui::tv_window_x,
+                            &config::ui::tv_window_y);
+   }
+   if (sdl::drcWindow) {
+      SDL_GetWindowPosition(sdl::drcWindow,
+                            &config::ui::drc_window_x,
+                            &config::ui::drc_window_y);
+   }
    SDL_Quit();
+}
+
+void
+activateContext()
+{
+   SDL_GL_MakeCurrent(sdl::tvWindow, glContext);
 }
 
 void
 swapBuffers()
 {
    SDL_GL_SwapWindow(sdl::tvWindow);
+   SDL_GL_SwapWindow(sdl::drcWindow);
 }
 
 void
-activateContext()
+bindDrcWindow()
 {
-   SDL_GL_MakeCurrent(sdl::tvWindow, tvContext);
+   SDL_GL_MakeCurrent(sdl::drcWindow, glContext);
+   gl::glViewport(0, 0, getDrcWidth(), getDrcHeight());
 }
 
 void
-releaseContext()
+bindTvWindow()
 {
-   SDL_GL_MakeCurrent(sdl::tvWindow, nullptr);
-}
-
-int
-getWindowWidth()
-{
-   return std::max(getTvWidth(), getDrcWidth());
-}
-
-int
-getWindowHeight()
-{
-   return getTvHeight() + getDrcHeight();
+   SDL_GL_MakeCurrent(sdl::tvWindow, glContext);
+   gl::glViewport(0, 0, getTvWidth(), getTvHeight());
 }
 
 int
