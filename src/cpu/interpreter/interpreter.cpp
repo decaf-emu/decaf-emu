586c05927ec97935a23a08b4a3bf83c29078cf92
James Benton <james.benton2@gmail.com>
2016-01-10 20:21:59 +0000

    Formatting.

--- src/cpu/interpreter/interpreter.cpp
+++ src/cpu/interpreter/interpreter.cpp
@@ -10,93 +10,93 @@
 
 namespace cpu
 {
+
 namespace interpreter
 {
 
-   static std::vector<instrfptr_t>
-   sInstructionMap;
+static std::vector<instrfptr_t>
+sInstructionMap;
 
-   void initialise()
-   {
-      sInstructionMap.resize(static_cast<size_t>(InstructionID::InstructionCount), nullptr);
+void initialise()
+{
+   sInstructionMap.resize(static_cast<size_t>(InstructionID::InstructionCount), nullptr);
 
-      // Register instruction handlers
-      registerBranchInstructions();
-      registerConditionInstructions();
-      registerFloatInstructions();
-      registerIntegerInstructions();
-      registerLoadStoreInstructions();
-      registerPairedInstructions();
-      registerSystemInstructions();
-   }
-
-   instrfptr_t getInstructionHandler(InstructionID id)
-   {
-      auto instrId = static_cast<size_t>(id);
-      if (instrId >= sInstructionMap.size()) {
-         return nullptr;
-      }
-      return sInstructionMap[instrId];
-   }
-
-   void registerInstruction(InstructionID id, instrfptr_t fptr)
-   {
-      sInstructionMap[static_cast<size_t>(id)] = fptr;
-   }
-
-   bool hasInstruction(InstructionID instrId)
-   {
-      return getInstructionHandler(instrId) != nullptr;
-   }
-
-   void execute(ThreadState *state)
-   {
-      while (state->nia != cpu::CALLBACK_ADDR) {
-         // TankTankTank decryptor fn
-         //forceJit = state->nia >= 0x0250B648 && state->nia < 0x0250B8B8;
-
-         if (state->core->interrupt.load()) {
-            cpu::gInterruptHandler(state->core, state);
-         }
-
-         // Interpreter Loop!
-         state->cia = state->nia;
-         state->nia = state->cia + 4;
-
-         gDebugControl.maybeBreak(state->cia, state, gProcessor.getCoreID());
-
-         auto instr = mem::read<Instruction>(state->cia);
-         auto data = gInstructionTable.decode(instr);
-
-         if (!data) {
-            gLog->error("Could not decode instruction at {:08x} = {:08x}", state->cia, instr.value);
-         }
-         assert(data);
-
-         auto trace = traceInstructionStart(instr, data, state);
-         auto fptr = sInstructionMap[static_cast<size_t>(data->id)];
-
-         if (!fptr) {
-            gLog->error("Unimplemented interpreter instruction {}", data->name);
-         }
-         assert(fptr);
-
-         fptr(state, instr);
-
-         traceInstructionEnd(trace, instr, data, state);
-      }
-   }
-
-
-   void executeSub(ThreadState *state)
-   {
-      auto lr = state->lr;
-      state->lr = CALLBACK_ADDR;
-
-      execute(state);
-
-      state->lr = lr;
-   }
-
+   // Register instruction handlers
+   registerBranchInstructions();
+   registerConditionInstructions();
+   registerFloatInstructions();
+   registerIntegerInstructions();
+   registerLoadStoreInstructions();
+   registerPairedInstructions();
+   registerSystemInstructions();
 }
+
+instrfptr_t getInstructionHandler(InstructionID id)
+{
+   auto instrId = static_cast<size_t>(id);
+
+   if (instrId >= sInstructionMap.size()) {
+      return nullptr;
+   }
+
+   return sInstructionMap[instrId];
 }
+
+void registerInstruction(InstructionID id, instrfptr_t fptr)
+{
+   sInstructionMap[static_cast<size_t>(id)] = fptr;
+}
+
+bool hasInstruction(InstructionID instrId)
+{
+   return getInstructionHandler(instrId) != nullptr;
+}
+
+void execute(ThreadState *state)
+{
+   while (state->nia != cpu::CALLBACK_ADDR) {
+      if (state->core->interrupt.load()) {
+         cpu::gInterruptHandler(state->core, state);
+      }
+
+      // Interpreter Loop!
+      state->cia = state->nia;
+      state->nia = state->cia + 4;
+
+      gDebugControl.maybeBreak(state->cia, state, gProcessor.getCoreID());
+
+      auto instr = mem::read<Instruction>(state->cia);
+      auto data = gInstructionTable.decode(instr);
+
+      if (!data) {
+         gLog->error("Could not decode instruction at {:08x} = {:08x}", state->cia, instr.value);
+      }
+      assert(data);
+
+      auto trace = traceInstructionStart(instr, data, state);
+      auto fptr = sInstructionMap[static_cast<size_t>(data->id)];
+
+      if (!fptr) {
+         gLog->error("Unimplemented interpreter instruction {}", data->name);
+      }
+      assert(fptr);
+
+      fptr(state, instr);
+      traceInstructionEnd(trace, instr, data, state);
+   }
+}
+
+
+void executeSub(ThreadState *state)
+{
+   auto lr = state->lr;
+   state->lr = CALLBACK_ADDR;
+
+   execute(state);
+
+   state->lr = lr;
+}
+
+} // namespace interpreter
+
+} // namespace cpu
