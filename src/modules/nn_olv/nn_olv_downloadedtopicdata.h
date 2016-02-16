83601fe2bf755f0fe0c571ed92e317f749f91fe8
James Benton <james.benton2@gmail.com>
2016-02-16 14:59:57 +0000

    nn_olv: Add member field offset checks for DownloadedTopicData.

--- src/modules/nn_olv/nn_olv_downloadedtopicdata.h
+++ src/modules/nn_olv/nn_olv_downloadedtopicdata.h
@@ -24,6 +24,12 @@ protected:
    be_val<uint32_t> mUnk1;
    be_val<uint32_t> mCommunityId;
    UNKNOWN(0xFF8);
+
+private:
+   CHECK_MEMBER_OFFSET_START
+   CHECK_OFFSET(DownloadedTopicData, 0x00, mUnk1);
+   CHECK_OFFSET(DownloadedTopicData, 0x04, mCommunityId);
+   CHECK_MEMBER_OFFSET_END
 };
 CHECK_SIZE(DownloadedTopicData, 0x1000);
 
