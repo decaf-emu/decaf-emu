38651ce23e09dbf11cfb395617c2dc7df591ae14
James Benton <james.benton2@gmail.com>
2016-05-24 21:44:35 +0100

    Fix gx2_draw.

--- src/modules/gx2/gx2_draw.cpp
+++ src/modules/gx2/gx2_draw.cpp
@@ -15,8 +15,13 @@ GX2SetAttribBuffer(uint32_t index,
    res.id = (index * 7) + latte::SQ_VS_ATTRIB_RESOURCE_0;
    res.baseAddress = buffer;
    res.size = size - 1;
-   res.word2.STRIDE = stride;
-   res.word6.TYPE = latte::SQ_TEX_VTX_VALID_BUFFER;
+
+   res.word2 = res.word2
+      .STRIDE().set(stride);
+
+   res.word6 = res.word6
+      .TYPE().set(latte::SQ_TEX_VTX_VALID_BUFFER);
+
    pm4::write(res);
 }
 
@@ -26,11 +31,16 @@ GX2DrawEx(GX2PrimitiveMode mode,
           uint32_t offset,
           uint32_t numInstances)
 {
+   auto vgt_dma_index_type = latte::VGT_DMA_INDEX_TYPE::get(0)
+      .INDEX_TYPE().set(static_cast<latte::VGT_INDEX>(GX2IndexType::U32));
+
+   auto vgt_draw_initiator = latte::VGT_DRAW_INITIATOR::get(0);
+
    pm4::write(pm4::SetControlConstant { latte::Register::SQ_VTX_BASE_VTX_LOC, offset });
    pm4::write(pm4::SetConfigReg { latte::Register::VGT_PRIMITIVE_TYPE, mode });
-   pm4::write(pm4::IndexType { GX2IndexType::U32 });
+   pm4::write(pm4::IndexType { vgt_dma_index_type });
    pm4::write(pm4::NumInstances { numInstances });
-   pm4::write(pm4::DrawIndexAuto { numVertices, 0 });
+   pm4::write(pm4::DrawIndexAuto { numVertices, vgt_draw_initiator });
 }
 
 void
@@ -41,11 +51,16 @@ GX2DrawIndexedEx(GX2PrimitiveMode mode,
                  uint32_t offset,
                  uint32_t numInstances)
 {
+   auto vgt_dma_index_type = latte::VGT_DMA_INDEX_TYPE::get(0)
+      .INDEX_TYPE().set(static_cast<latte::VGT_INDEX>(indexType));
+
+   auto vgt_draw_initiator = latte::VGT_DRAW_INITIATOR::get(0);
+
    pm4::write(pm4::SetControlConstant { latte::Register::SQ_VTX_BASE_VTX_LOC, offset });
    pm4::write(pm4::SetConfigReg { latte::Register::VGT_PRIMITIVE_TYPE, mode });
-   pm4::write(pm4::IndexType { indexType });
+   pm4::write(pm4::IndexType { vgt_dma_index_type });
    pm4::write(pm4::NumInstances { numInstances });
-   pm4::write(pm4::DrawIndex2 { static_cast<uint32_t>(-1), indices, numVertices, 0 });
+   pm4::write(pm4::DrawIndex2 { static_cast<uint32_t>(-1), indices, numVertices, vgt_draw_initiator });
 }
 
 void
