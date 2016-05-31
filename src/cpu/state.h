21a3e0e3a4e2ddbfe2a999f6998bd2b6682a2327
James Benton <james.benton2@gmail.com>
2016-05-31 04:16:58 -0700

    Fix CPU state using non-standard std::atomic_uint32_t.
    Use std::atomic<uint32_t> instead.

--- src/cpu/state.h
+++ src/cpu/state.h
@@ -51,9 +51,8 @@ struct Core
    // State data used by the CPU executors
    uint32_t id;
    std::thread thread;
-   std::atomic_uint32_t interrupt{ 0 };
+   std::atomic<uint32_t> interrupt{ 0 };
    std::chrono::system_clock::time_point next_alarm;
-
 };
 
 }
