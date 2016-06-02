7c738860665135b8f28f41c072d55d0e4999cac5
James Benton <james.benton2@gmail.com>
2016-06-02 02:13:35 +0100

    coreinit: Add coreinit::internal::IdLock.
    This is a simple spin lock which does nothing fancy.

---
+++ src/modules/coreinit/coreinit_internal_idlock.cpp
@@ -0,0 +1,52 @@
+#include "coreinit_internal_idlock.h"
+#include "memory_translate.h"
+
+namespace coreinit
+{
+
+namespace internal
+{
+
+void
+acquireIdLock(IdLock &lock, uint32_t id)
+{
+   uint32_t expected = 0;
+
+   while (!lock.owner.compare_exchange_weak(expected, id, std::memory_order_acquire)) {
+      expected = 0;
+   }
+}
+
+void
+releaseIdLock(IdLock &lock, uint32_t id)
+{
+   lock.owner.store(0, std::memory_order_release);
+}
+
+void
+acquireIdLock(IdLock &lock, void *owner)
+{
+   acquireIdLock(lock, memory_untranslate(owner));
+}
+
+void
+releaseIdLock(IdLock &lock, void *owner)
+{
+   releaseIdLock(lock, memory_untranslate(owner));
+}
+
+void
+acquireIdLock(IdLock &lock)
+{
+   acquireIdLock(lock, 0xDEADBEEF);
+}
+
+void
+releaseIdLock(IdLock &lock)
+{
+   releaseIdLock(lock, 0xDEADBEEF);
+}
+
+} // namespace internal
+
+} // namespace coreinit
