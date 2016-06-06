fef5eea2e5bce9e3ab06ebeda6d208f812b6cd33
Brett Lawson <brett19@gmail.com>
2016-06-06 01:12:10 -0700

    Move all Core registers to their own structure.

--- src/cpu/state.h
+++ src/cpu/state.h
@@ -7,44 +7,37 @@ struct Tracer;
 
 namespace cpu {
 
-struct Core
+struct CoreRegs
 {
-   // TODO: Remove these
-   using gpr_t = espresso::gpr_t;
-   using fpr_t = espresso::fpr_t;
-   using cr_t = espresso::cr_t;
-   using xer_t = espresso::xer_t;
-   using fpscr_t = espresso::fpscr_t;
-   using pvr_t = espresso::pvr_t;
-   using msr_t = espresso::msr_t;
-   using gqr_t = espresso::gqr_t;
+   uint32_t cia;              // Current execution address
+   uint32_t nia;              // Next execution address
 
-   uint32_t cia;     // Current execution address
-   uint32_t nia;     // Next execution address
+   espresso::gpr_t gpr[32];   // Integer Registers
+   espresso::fpr_t fpr[32];   // Floating-point Registers
+   espresso::cr_t cr;         // Condition Register
+   espresso::xer_t xer;       // XER Carry/Overflow register
+   uint32_t lr;               // Link Register
+   uint32_t ctr;              // Count Register
 
-   gpr_t gpr[32];    // Integer Registers
-   fpr_t fpr[32];    // Floating-point Registers
-   cr_t cr;          // Condition Register
-   xer_t xer;        // XER Carry/Overflow register
-   uint32_t lr;      // Link Register
-   uint32_t ctr;     // Count Register
+   espresso::fpscr_t fpscr;   // Floating-Point Status and Control Register
 
-   fpscr_t fpscr;    // Floating-Point Status and Control Register
+   espresso::pvr_t pvr;       // Processor Version Register
+   espresso::msr_t msr;       // Machine State Register
+   uint32_t sr[16];           // Segment Registers
 
-   pvr_t pvr;        // Processor Version Register
-   msr_t msr;        // Machine State Register
-   uint32_t sr[16];  // Segment Registers
+   uint32_t tbu;              // Time Base Upper
+   uint32_t tbl;              // Time Base Lower
 
-   uint32_t tbu;     // Time Base Upper
-   uint32_t tbl;     // Time Base Lower
+   espresso::gqr_t gqr[8];    // Graphics Quantization Registers
 
-   gqr_t gqr[8];     // Graphics Quantization Registers
-
-   // Reserve data for lwarx / stwcx.
+                              // Reserve data for lwarx / stwcx.
    bool reserve;
    uint32_t reserveAddress;
    uint32_t reserveData;
+};
 
+struct Core : CoreRegs
+{
    // Tracer used to record executed instructions
    Tracer *tracer;
 
