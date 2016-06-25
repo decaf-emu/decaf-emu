49cb58e86498ed7319e952cc68246f1dad6063ae
Brett Lawson <brett19@gmail.com>
2016-06-25 01:41:25 -0700

    Added some helpful assertions to wfunc_call.

--- src/libdecaf/src/ppcutils/wfunc_call.h
+++ src/libdecaf/src/ppcutils/wfunc_call.h
@@ -8,8 +8,13 @@ ReturnType wfunc_ptr<ReturnType, Args...>::operator()(Args... args)
 {
    auto core = cpu::this_core::state();
 
+   // TODO: ppctypes should use CoreRegs& not Core*
+
    // Push args
    ppctypes::applyArguments(core, args...);
+
+   // For debugging, lets grab a copy of the SP
+   auto origStackPtr = core->gpr[1];
 
    // Allocate callee backchain and lr space.
    core->gpr[1] -= 2 * 4;
@@ -17,16 +22,24 @@ ReturnType wfunc_ptr<ReturnType, Args...>::operator()(Args... args)
    // Save state
    auto nia = core->nia;
 
-   // Set state
+   // Set state to our function to call
    core->cia = 0;
    core->nia = address;
+
+   // Start executing!
    cpu::this_core::executeSub();
+
+   // Grab the most recent core state as it may have changed.
+   core = cpu::this_core::state();
 
    // Restore state
    core->nia = nia;
 
    // Restore callee args stack space
    core->gpr[1] += 2 * 4;
+
+   // Lets verify we did not corrupt the SP
+   emuassert(core->gpr[1] == origStackPtr);
 
    // Return the result
    return ppctypes::getResult<ReturnType>(core);
