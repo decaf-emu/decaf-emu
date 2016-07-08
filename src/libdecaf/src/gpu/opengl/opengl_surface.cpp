992153b1644ddb211973fb69f2c554ba24b1a2f3
Brett Lawson <brett19@gmail.com>
2016-07-08 01:32:18 -0700

    Implement FMT_2_10_10_10 using GL_RGB10_A2.
    This is valid as both byte-orders have RGB being 10 bits
    and A being 2.  The only place that these formats will
    differ is when we are loading/saving from PPC memory.
    Which is already handled by the opengl_texture.cpp's
    StorageFormat behaviour.

--- src/libdecaf/src/gpu/opengl/opengl_surface.cpp
+++ src/libdecaf/src/gpu/opengl/opengl_surface.cpp
@@ -95,6 +95,9 @@ getStorageFormat(latte::SQ_DATA_FORMAT format,
    //case latte::SQ_DATA_FORMAT::FMT_11_11_10:
    case latte::SQ_DATA_FORMAT::FMT_11_11_10_FLOAT:
       return gl::GL_R11F_G11F_B10F;
+   case latte::SQ_DATA_FORMAT::FMT_2_10_10_10:
+      return getStorageFormat(numFormat, formatComp, degamma,
+         gl::GL_RGB10_A2, BADFMT, gl::GL_RGB10_A2UI, BADFMT, BADFMT);
    case latte::SQ_DATA_FORMAT::FMT_8_8_8_8:
       return getStorageFormat(numFormat, formatComp, degamma,
          gl::GL_RGBA8, gl::GL_RGBA8_SNORM, gl::GL_RGBA8UI, gl::GL_RGBA8I, gl::GL_SRGB8);
