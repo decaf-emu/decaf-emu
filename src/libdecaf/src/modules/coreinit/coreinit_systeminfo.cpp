be7258691707a17c8a057cf33284ec66f169bbc4
Brett Lawson <brett19@gmail.com>
2016-07-08 01:25:21 -0700

    Implemented bspGetHardwareVersion according to WiiUBrew.

--- src/libdecaf/src/modules/coreinit/coreinit_systeminfo.cpp
+++ src/libdecaf/src/modules/coreinit/coreinit_systeminfo.cpp
@@ -8,6 +8,47 @@
 namespace coreinit
 {
 
+enum class HardwareVersion : uint32_t
+{
+   UNKNOWN = 0x00000000,
+
+   // vWii Hardware Versions
+   HOLLYWOOD_ENG_SAMPLE_1 = 0x00000001,
+   HOLLYWOOD_ENG_SAMPLE_2 = 0x10000001,
+   HOLLYWOOD_PROD_FOR_WII = 0x10100001,
+   HOLLYWOOD_CORTADO = 0x10100008,
+   HOLLYWOOD_CORTADO_ESPRESSO = 0x1010000C,
+   BOLLYWOOD = 0x20000001,
+   BOLLYWOOD_PROD_FOR_WII = 0x20100001,
+
+   // WiiU Hardware Versions
+   LATTE_A11_EV = 0x21100010,
+   LATTE_A11_CAT = 0x21100020,
+   LATTE_A12_EV = 0x21200010,
+   LATTE_A12_CAT = 0x21200020,
+
+   LATTE_A2X_EV = 0x22100010,
+   LATTE_A2X_CAT = 0x22100020,
+
+   LATTE_A3X_EV = 0x23100010,
+   LATTE_A3X_CAT = 0x23100020,
+   LATTE_A3X_CAFE = 0x23100028,
+
+   LATTE_A4X_EV = 0x24100010,
+   LATTE_A4X_CAT = 0x24100020,
+   LATTE_A4X_CAFE = 0x24100028,
+
+   LATTE_A5X_EV = 0x25100010,
+   LATTE_A5X_EV_Y = 0x25100011,
+   LATTE_A5X_CAT = 0x25100020,
+   LATTE_A5X_CAFE = 0x25100028,
+
+   LATTE_B1X_EV = 0x26100010,
+   LATTE_B1X_EV_Y = 0x26100011,
+   LATTE_B1X_CAT = 0x26100020,
+   LATTE_B1X_CAFE = 0x26100028,
+};
+
 static OSSystemInfo *
 sSystemInfo = nullptr;
 
@@ -22,6 +63,12 @@ sTitleID = 0;
 
 static uint64_t
 sSystemID = 0x000500101000400Aull;
+
+HardwareVersion
+bspGetHardwareVersion()
+{
+   return HardwareVersion::LATTE_B1X_CAFE;
+}
 
 OSSystemInfo *
 OSGetSystemInfo()
@@ -101,6 +148,7 @@ Module::initialiseSystemInformation()
 void
 Module::registerSystemInfoFunctions()
 {
+   RegisterKernelFunction(bspGetHardwareVersion);
    RegisterKernelFunction(OSGetSystemInfo);
    RegisterKernelFunction(OSSetScreenCapturePermission);
    RegisterKernelFunction(OSGetScreenCapturePermission);
