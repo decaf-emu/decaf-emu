decaf-emu/decaf-emu

commit 73b6a6e68d776f04274360e9a744d4784beff3af

common: Print stacktrace when an assert is hit.

src/common/src/assert.cpp
@@ -1,6 +1,7 @@
 #include "decaf_assert.h"
 #include "log.h"
 #include "platform.h"
+#include "platform_stacktrace.h"
 #include <iostream>
 
 #ifdef PLATFORM_WINDOWS
@@ -15,6 +16,10 @@ assertFailed(const char *file,
              const char *expression,
              const std::string &message)
 {
+   auto stackTrace = platform::captureStackTrace();
+   auto trace = platform::formatStackTrace(stackTrace);
+   platform::freeStackTrace(stackTrace);
+
    fmt::MemoryWriter out;
    out << "Assertion failed:\n";
    out << "Expression: " << expression << "\n";
@@ -23,6 +28,10 @@ assertFailed(const char *file,
 
    if (!message.empty()) {
       out << "Message: " << message << "\n";
+   }
+
+   if (trace.size()) {
+      out << "Stacktrace:\n" << trace << "\n";
    }
 
    if (gLog) {

src/common/platform_stacktrace.h
@@ -12,6 +12,9 @@ captureStackTrace();
 void
 freeStackTrace(StackTrace *trace);
 
+std::string
+formatStackTrace(StackTrace *trace);
+
 void
 printStackTrace(StackTrace *trace);
 

src/common/src/platform_posix_stacktrace.cpp
@@ -17,6 +17,12 @@ void freeStackTrace(StackTrace *)
 {
 }
 
+std::string
+formatStackTrace(StackTrace *trace)
+{
+   return { };
+}
+
 void printStackTrace(StackTrace *)
 {
    decaf_abort("POSIX support for stack tracing is not implemented");

src/common/src/platform_win_stacktrace.cpp
@@ -1,5 +1,6 @@
 #include "platform.h"
 #include "platform_stacktrace.h"
+#include <spdlog/details/format.h>
 
 #ifdef PLATFORM_WINDOWS
 #include <Windows.h>
@@ -33,30 +34,37 @@ freeStackTrace(StackTrace *trace)
    delete trace;
 }
 
-void
-printStackTrace(StackTrace *trace)
+std::string
+formatStackTrace(StackTrace *trace)
 {
    auto process = GetCurrentProcess();
-
    static bool symInitialise = false;
+
    if (!symInitialise) {
       SymInitialize(process, NULL, TRUE);
       symInitialise = true;
    }
 
-   // stack allocation neccessary according to MSDN
    auto symbol = new MySymbol();
    symbol->MaxNameLen = 256;
    symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
 
-   char printBuf[0x1024];
-   for (uint16_t i = 0; i < trace->frames; ++i) {
+   fmt::MemoryWriter out;
+
+   for (auto i = 0u; i < trace->frames; ++i) {
       SymFromAddr(process, (DWORD64)trace->data[i], 0, symbol);
-      sprintf_s(printBuf, "%i: %s - 0x%0I64x\n", trace->frames - i - 1, (const char*)symbol->Name, symbol->Address);
-      OutputDebugStringA(printBuf);
+      out.write("{}: {} - 0x{:X}x\n", trace->frames - i - 1, (const char*)symbol->Name, symbol->Address);
    }
 
    delete symbol;
+   return out.str();
+}
+
+void
+printStackTrace(StackTrace *trace)
+{
+   auto str = formatStackTrace(trace);
+   OutputDebugStringA(str.c_str());
 }
 
 } // namespace platform

commit 8b008f6352b06b200dad0d09ddc2e5eb7d0e6111

Fix compilation on Windows
Broken by 73b6a6e68d776f04274360e9a744d4784beff3af

src/common/src/assert.cpp
@@ -8,6 +8,8 @@
 #include <cassert>
 #include <codecvt>
 #include <Windows.h>
+
+#pragma comment(lib, "dbghelp.lib")
 #endif
 
 void