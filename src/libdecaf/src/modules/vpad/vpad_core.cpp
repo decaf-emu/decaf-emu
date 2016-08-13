45ab584831f753d90e5821d2fbac2553a3c8f078
James Benton <james.benton2@gmail.com>
2016-08-13 02:18:26 +0100

    vpad: Stub VPADControlMotor.

--- src/libdecaf/src/modules/vpad/vpad_core.cpp
+++ src/libdecaf/src/modules/vpad/vpad_core.cpp
@@ -19,12 +19,19 @@ VPADSetBtnRepeat(uint32_t chan, float unk1, float unk2)
 {
 }
 
+int
+VPADControlMotor(uint32_t chan, uint8_t *buffer, uint32_t size)
+{
+   return 0;
+}
+
 void
 Module::registerCoreFunctions()
 {
    RegisterKernelFunction(VPADInit);
    RegisterKernelFunction(VPADSetAccParam);
    RegisterKernelFunction(VPADSetBtnRepeat);
+   RegisterKernelFunction(VPADControlMotor);
 }
 
 } // namespace vpad
--- src/libdecaf/src/modules/vpad/vpad_core.h
+++ src/libdecaf/src/modules/vpad/vpad_core.h
@@ -13,4 +13,7 @@ VPADSetAccParam(uint32_t chan, float unk1, float unk2);
 void
 VPADSetBtnRepeat(uint32_t chan, float unk1, float unk2);
 
+int
+VPADControlMotor(uint32_t chan, uint8_t *buffer, uint32_t size);
+
 } // namespace vpad
