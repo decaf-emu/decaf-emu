7db1960114ad3fa4bbd227ae9d071478db875ec0
James Benton <james.benton2@gmail.com>
2016-08-15 01:42:58 +0100

    coreinit: Add input device register read/write.

--- src/libdecaf/src/modules/coreinit/coreinit_device.cpp
+++ src/libdecaf/src/modules/coreinit/coreinit_device.cpp
@@ -1,11 +1,22 @@
 #include "coreinit.h"
 #include "coreinit_device.h"
+#include "coreinit_enum_string.h"
+#include "common/bitfield.h"
 
 namespace coreinit
 {
 
+static OSInputDevice
+sInputDevice = { 0 };
+
 static BOOL
-OSDriver_Register(uint32_t r3, uint32_t r4, void *somePtr, uint32_t someID, uint32_t r7, uint32_t *someOutValue, uint32_t r9)
+OSDriver_Register(uint32_t r3,
+                  uint32_t r4,
+                  void *r5,
+                  uint32_t r6,
+                  uint32_t r7,
+                  uint32_t *r8,
+                  uint32_t r9)
 {
    // TODO: OSDriver_Register
    return FALSE;
@@ -18,23 +29,97 @@ OSEnforceInorderIO()
 }
 
 uint16_t
-OSReadRegister16(uint32_t device, uint32_t id)
+OSReadRegister16(OSDeviceID device,
+                 uint32_t id)
 {
-   // TODO: OSReadRegister16
-   return 0;
+   return OSReadRegister32Ex(device, id) & 0xFFFF;
 }
 
 uint32_t
-OSReadRegister32Ex(uint32_t device, uint32_t id)
+OSReadRegister32Ex(OSDeviceID device,
+                   uint32_t id)
 {
-   // TODO: OSReadRegister32Ex
+   if (device == OSDeviceID::Input) {
+      auto reg = static_cast<OSDeviceInputRegisters>(id);
+
+      switch (reg) {
+      case OSDeviceInputRegisters::DeviceStatus:
+         return sInputDevice.deviceStatus.value;
+      case OSDeviceInputRegisters::ControllerError:
+         return sInputDevice.controllerError.value;
+      case OSDeviceInputRegisters::PollControl:
+         return sInputDevice.pollControl.value;
+      case OSDeviceInputRegisters::Controller0Command:
+         return sInputDevice.controllers[0].command.value;
+      case OSDeviceInputRegisters::Controller1Command:
+         return sInputDevice.controllers[1].command.value;
+      case OSDeviceInputRegisters::Controller2Command:
+         return sInputDevice.controllers[2].command.value;
+      case OSDeviceInputRegisters::Controller3Command:
+         return sInputDevice.controllers[3].command.value;
+      case OSDeviceInputRegisters::Controller0Status0:
+      case OSDeviceInputRegisters::Controller1Status0:
+      case OSDeviceInputRegisters::Controller2Status0:
+      case OSDeviceInputRegisters::Controller3Status0:
+         return OSInputDevice::ControllerStatus0::get(0)
+            .error(false)
+            .stickLX(-0x80)
+            .stickLY(-0x80)
+            .value;
+      case OSDeviceInputRegisters::Controller0Status1:
+      case OSDeviceInputRegisters::Controller1Status1:
+      case OSDeviceInputRegisters::Controller2Status1:
+      case OSDeviceInputRegisters::Controller3Status1:
+         return OSInputDevice::ControllerStatus1::get(0)
+            .stickRX(-0x80)
+            .stickRY(-0x80)
+            .value;
+      default:
+         gLog->warn("OSReadRegister - Unimplemented device {} register {}", enumAsString(device), enumAsString(reg));
+         return 0;
+      }
+   }
+
+   gLog->warn("OSReadRegister - Unimplemented device {} register {}", enumAsString(device), id);
    return 0;
 }
 
 void
-OSWriteRegister32Ex(uint32_t device, uint32_t id, uint32_t value)
+OSWriteRegister32Ex(OSDeviceID device,
+                    uint32_t id,
+                    uint32_t value)
 {
-   // TODO: OSWriteRegister32Ex
+   if (device == OSDeviceID::Input) {
+      auto reg = static_cast<OSDeviceInputRegisters>(id);
+
+      switch (id) {
+      case OSDeviceInputRegisters::Controller0Command:
+         sInputDevice.controllers[0].command = OSInputDevice::ControllerCommand::get(value);
+         break;
+      case OSDeviceInputRegisters::Controller1Command:
+         sInputDevice.controllers[1].command = OSInputDevice::ControllerCommand::get(value);
+         break;
+      case OSDeviceInputRegisters::Controller2Command:
+         sInputDevice.controllers[2].command = OSInputDevice::ControllerCommand::get(value);
+         break;
+      case OSDeviceInputRegisters::Controller3Command:
+         sInputDevice.controllers[3].command = OSInputDevice::ControllerCommand::get(value);
+         break;
+      case OSDeviceInputRegisters::PollControl:
+         sInputDevice.pollControl = OSInputDevice::PollControl::get(value);
+         break;
+      case OSDeviceInputRegisters::DeviceStatus:
+         sInputDevice.deviceStatus = OSInputDevice::DeviceStatus::get(value);
+         break;
+      case OSDeviceInputRegisters::ControllerError:
+         sInputDevice.controllerError = OSInputDevice::ControllerError::get(value);
+         break;
+      default:
+         gLog->warn("OSWriteRegister - Unimplemented device {} register {} = 0x{:08X}", enumAsString(device), enumAsString(reg), value);
+      }
+   } else {
+      gLog->warn("OSWriteRegister - Unimplemented device {} register {} = 0x{:08X}", enumAsString(device), id, value);
+   }
 }
 
 void
--- src/libdecaf/src/modules/coreinit/coreinit_device.h
+++ src/libdecaf/src/modules/coreinit/coreinit_device.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "common/bitfield.h"
 #include "common/types.h"
+#include "coreinit_enum.h"
 
 namespace coreinit
 {
@@ -10,14 +12,85 @@ namespace coreinit
  * @{
  */
 
+#pragma pack(push, 1)
+
+struct OSInputDevice
+{
+   BITFIELD(DeviceStatus, uint32_t)
+      BITFIELD_ENTRY(0, 1, bool, device_busy);
+   BITFIELD_END
+
+   BITFIELD(PollControl, uint32_t)
+      BITFIELD_ENTRY(4, 1, bool, pollController3);
+      BITFIELD_ENTRY(5, 1, bool, pollController2);
+      BITFIELD_ENTRY(6, 1, bool, pollController1);
+      BITFIELD_ENTRY(7, 1, bool, pollController0);
+   BITFIELD_END
+
+   BITFIELD(ControllerError, uint32_t)
+      BITFIELD_ENTRY(0, 4, uint8_t, controllerError3);
+      BITFIELD_ENTRY(8, 4, uint8_t, controllerError2);
+      BITFIELD_ENTRY(16, 4, uint8_t, controllerError1);
+      BITFIELD_ENTRY(24, 4, uint8_t, controllerError0);
+   BITFIELD_END
+
+   BITFIELD(ControllerCommand, uint32_t)
+   BITFIELD_END
+
+   BITFIELD(ControllerStatus0, uint32_t)
+      BITFIELD_ENTRY(0, 8, int8_t, stickLY);
+      BITFIELD_ENTRY(8, 8, int8_t, stickLX);
+      BITFIELD_ENTRY(16, 1, bool, btnLeft);
+      BITFIELD_ENTRY(17, 1, bool, btnRight);
+      BITFIELD_ENTRY(18, 1, bool, btnDown);
+      BITFIELD_ENTRY(19, 1, bool, btnUp);
+      BITFIELD_ENTRY(20, 1, bool, btnTriggerZ);
+      BITFIELD_ENTRY(21, 1, bool, btnTriggerR);
+      BITFIELD_ENTRY(22, 1, bool, btnTriggerL);
+      BITFIELD_ENTRY(23, 1, bool, btnA);
+      BITFIELD_ENTRY(24, 1, bool, btnB);
+      BITFIELD_ENTRY(25, 1, bool, btnX);
+      BITFIELD_ENTRY(26, 1, bool, btnY);
+      BITFIELD_ENTRY(27, 1, bool, btnStart);
+      // 28, 39 is included in the &0x3FFF bitmask for buttons, but dunno what buttons they are
+      // 30 : unk
+      BITFIELD_ENTRY(31, 1, bool, error);
+   BITFIELD_END
+
+   BITFIELD(ControllerStatus1, uint32_t)
+      BITFIELD_ENTRY(0, 8, uint8_t, analogB);
+      BITFIELD_ENTRY(8, 8, uint8_t, analogA);
+      BITFIELD_ENTRY(16, 8, int8_t, stickRY);
+      BITFIELD_ENTRY(24, 8, int8_t, stickRX);
+   BITFIELD_END
+
+   struct ControllerRegisters
+   {
+      ControllerCommand command;
+      ControllerStatus0 status0;
+      ControllerStatus1 status1;
+   };
+
+   ControllerRegisters controllers[4];
+   PollControl pollControl;
+   DeviceStatus deviceStatus;
+   ControllerError controllerError;
+};
+
+#pragma pack(pop)
+
 uint16_t
-OSReadRegister16(uint32_t device, uint32_t id);
+OSReadRegister16(OSDeviceID device,
+                 uint32_t id);
 
 uint32_t
-OSReadRegister32Ex(uint32_t device, uint32_t id);
+OSReadRegister32Ex(OSDeviceID device,
+                   uint32_t id);
 
 void
-OSWriteRegister32Ex(uint32_t device, uint32_t id, uint32_t value);
+OSWriteRegister32Ex(OSDeviceID device,
+                    uint32_t id,
+                    uint32_t value);
 
 /** @} */
 
