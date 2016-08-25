12efbc4cba202dff2bc0a7061600655097899947
James Benton <james.benton2@gmail.com>
2016-08-25 02:00:31 +0100

    tests/hle: Add simple thread cancel test.

---
+++ tests/hle/coreinit/thread/thread_cancel.c
@@ -0,0 +1,46 @@
+#include <hle_test.h>
+#include <coreinit/mutex.h>
+#include <coreinit/systeminfo.h>
+#include <coreinit/thread.h>
+#include <coreinit/time.h>
+
+OSThread gThread;
+OSMutex gMutex;
+uint8_t gThreadStack[4096];
+
+int cancelThreadEntry(int argc, const char **argv)
+{
+   OSTime lastCheck = OSGetTime();
+
+   while (1) {
+      OSTime now = OSGetTime();
+
+      if (now - lastCheck >= OSMilliseconds(1)) {
+         OSTestThreadCancel();
+         test_report("Cancel thread still running");
+         lastCheck = now;
+      }
+   }
+
+   return 0;
+}
+
+int main(int argc, char **argv)
+{
+   test_report("Setup");
+   OSInitMutex(&gMutex);
+   OSCreateThread(&gThread, cancelThreadEntry, 0, NULL, gThreadStack + 4096, 4096, 20, 0);
+
+   test_report("Start the thread");
+   OSResumeThread(&gThread);
+
+   test_report("Main thread sleeping for 5 milliseconds");
+   OSSleepTicks(OSMilliseconds(5));
+
+   test_report("Try to cancel thread");
+   OSCancelThread(&gThread);
+   OSJoinThread(&gThread, NULL);
+
+   test_report("Cancel thread succeeded");
+   return 0;
+}
