bc395215f51f9e3678e154f71e228ee7c516466d
Brett Lawson <brett19@gmail.com>
2016-08-25 20:44:23 -0700

    Fix issue with FFLStoreData type definition.
    This broke linux builds. Oops.

--- src/libdecaf/src/modules/nn_ffl.h
+++ src/libdecaf/src/modules/nn_ffl.h
@@ -11,7 +11,7 @@ struct FFLStoreData
    uint8_t deviceHash[6];
    uint16_t unk2;
    uint16_t unk3;
-   wchar_t name[10];
+   char16_t name[10];
    uint16_t unk4;
    uint32_t unk5;
    uint32_t unk6;
@@ -19,7 +19,7 @@ struct FFLStoreData
    uint32_t unk8;
    uint32_t unk9;
    uint32_t unk10;
-   wchar_t creator[10];
+   char16_t creator[10];
    uint32_t unk11;
 };
 CHECK_SIZE(FFLStoreData, 96);
