0930753255924caee2bf59beeea5b4d6e72b25c5
James Benton <james.benton2@gmail.com>
2016-09-02 02:01:45 +0100

    gx2: Minor formatting.

--- src/libdecaf/src/modules/gx2/gx2r_resource.h
+++ src/libdecaf/src/modules/gx2/gx2r_resource.h
@@ -10,7 +10,8 @@ using GX2RAllocFuncPtr = wfunc_ptr<void *, GX2RResourceFlags, uint32_t, uint32_t
 using GX2RFreeFuncPtr = wfunc_ptr<void, GX2RResourceFlags, void *>;
 
 void
-GX2RSetAllocator(GX2RAllocFuncPtr allocFn, GX2RFreeFuncPtr freeFn);
+GX2RSetAllocator(GX2RAllocFuncPtr allocFn,
+                 GX2RFreeFuncPtr freeFn);
 
 BOOL
 GX2RIsUserMemory(GX2RResourceFlags flags);
@@ -19,16 +20,22 @@ namespace internal
 {
 
 void *
-gx2rAlloc(GX2RResourceFlags flags, uint32_t size, uint32_t align);
+gx2rAlloc(GX2RResourceFlags flags,
+          uint32_t size,
+          uint32_t align);
 
 void
-gx2rFree(GX2RResourceFlags flags, void *buffer);
+gx2rFree(GX2RResourceFlags flags,
+         void *buffer);
 
 void *
-gx2rDefaultAlloc(GX2RResourceFlags flags, uint32_t size, uint32_t align);
+gx2rDefaultAlloc(GX2RResourceFlags flags,
+                 uint32_t size,
+                 uint32_t align);
 
 void
-gx2rDefaultFree(GX2RResourceFlags flags, void *buffer);
+gx2rDefaultFree(GX2RResourceFlags flags,
+                void *buffer);
 
 } // namespace internal
 
--- src/libdecaf/src/modules/gx2/gx2r_shaders.h
+++ src/libdecaf/src/modules/gx2/gx2r_shaders.h
@@ -5,12 +5,18 @@ namespace gx2
 {
 
 void
-GX2RSetVertexUniformBlock(GX2RBuffer* buffer, uint32_t location, uint32_t offset);
+GX2RSetVertexUniformBlock(GX2RBuffer* buffer,
+                          uint32_t location,
+                          uint32_t offset);
 
 void
-GX2RSetPixelUniformBlock(GX2RBuffer* buffer, uint32_t location, uint32_t offset);
+GX2RSetPixelUniformBlock(GX2RBuffer* buffer,
+                         uint32_t location,
+                         uint32_t offset);
 
 void
-GX2RSetGeometryUniformBlock(GX2RBuffer* buffer, uint32_t location, uint32_t offset);
+GX2RSetGeometryUniformBlock(GX2RBuffer* buffer,
+                            uint32_t location,
+                            uint32_t offset);
 
 } // namespace gx2
