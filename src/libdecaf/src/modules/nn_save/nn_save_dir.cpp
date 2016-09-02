6ca91685610cf8d9c42e2ca4a7cbb2f4fac1c8b2
James Benton <james.benton2@gmail.com>
2016-09-02 01:48:38 +0100

    nn_save: Fix save directory.

--- src/libdecaf/src/modules/nn_save/nn_save_dir.cpp
+++ src/libdecaf/src/modules/nn_save/nn_save_dir.cpp
@@ -21,7 +21,7 @@ SAVEInitSaveDir(uint8_t userID)
    auto titleHi = static_cast<uint32_t>(titleID >> 32);
 
    // Create title save folder
-   auto path = fmt::format("/vol/storage_mlc01/sys/title/{:08x}/{:08x}/user", titleHi, titleLo);
+   auto path = fmt::format("/vol/storage_mlc01/usr/save/{:08x}/{:08x}/user", titleHi, titleLo);
    auto titleFolder = fs->makeFolder(path);
 
    if (!titleFolder) {
@@ -33,7 +33,7 @@ SAVEInitSaveDir(uint8_t userID)
       return SaveStatus::FatalError;
    }
 
-   // Create user save folder
+   // Create current user save folder
    auto savePath = internal::getSaveDirectory(userID);
 
    if (!fs->makeFolder(savePath)) {
