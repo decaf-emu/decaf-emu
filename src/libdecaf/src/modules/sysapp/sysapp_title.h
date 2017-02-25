92be798e1b1f3b3de25f3d80fa886623307136ce
James Benton <james.benton2@gmail.com>
2017-02-25 11:49:43 +0000

    sysapp: Move SystemAppId to sysapp_enum.h

--- src/libdecaf/src/modules/sysapp/sysapp_title.h
+++ src/libdecaf/src/modules/sysapp/sysapp_title.h
@@ -1,26 +1,11 @@
 #pragma once
-#include "common/types.h"
 #include "modules/coreinit/coreinit_enum.h"
+#include "sysapp_enum.h"
+
+#include <cstdint>
 
 namespace sysapp
 {
-
-enum SystemAppId : uint32_t
-{
-   Updater                 = 0,
-   SystemSettings          = 1,
-   ParentalControls        = 2,
-   UserSettings            = 3,
-   MiiMaker                = 4,
-   AccountSettings         = 5,
-   DailyLog                = 6,
-   Notifications           = 7,
-   HealthAndSafety         = 8,
-   ElectronicManual        = 9,
-   WiiUChat                = 10,
-   SoftwareDataTransfer    = 11,
-   Max,
-};
 
 uint64_t
 SYSGetSystemApplicationTitleId(SystemAppId id);
