bb9a7638d5dddeee8092c4c126e07b0e78ed23bb
James Benton <james.benton2@gmail.com>
2017-03-23 18:21:35 +0000

    be_ptr: Fix implicit integer cast.

--- src/common/be_ptr.h
+++ src/common/be_ptr.h
@@ -81,14 +81,14 @@ public:
    be_ptr
    operator +(T offset) const
    {
-      return { mem::translate<Type>(getAddress() + offset * sizeof(Type)) };
+      return { mem::translate<Type>(getAddress() + static_cast<uint32_t>(offset * sizeof(Type))) };
    }
 
    template<typename T>
    be_ptr
    operator -(T offset) const
    {
-      return { mem::translate<Type>(getAddress() - offset * sizeof(Type)) };
+      return { mem::translate<Type>(getAddress() - static_cast<uint32_t>(offset * sizeof(Type))) };
    }
 
    template<typename T>
