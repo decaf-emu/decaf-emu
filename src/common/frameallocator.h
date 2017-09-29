debf0df92ea63c514b007424ed462bd8cd0bee5e
James Benton <james.benton2@gmail.com>
2017-09-29 11:46:54 +0100

    common: Add FrameAllocator default constructor.
    Also bool empty() and reset().

--- src/common/frameallocator.h
+++ src/common/frameallocator.h
@@ -6,11 +6,24 @@
 class FrameAllocator
 {
 public:
+   FrameAllocator() :
+      mBase(nullptr),
+      mSize(0),
+      mOffset(0)
+   {
+   }
+
    FrameAllocator(void *base, size_t size) :
       mBase(reinterpret_cast<uint8_t *>(base)),
       mSize(size),
       mOffset(0)
    {
+   }
+
+   bool
+   empty() const
+   {
+      return mOffset == 0;
    }
 
    uint8_t *
@@ -34,6 +47,12 @@ public:
       auto result = top() + alignOffset;
       mOffset += size;
       return result;
+   }
+
+   void
+   reset()
+   {
+      mOffset = 0;
    }
 
 protected:
