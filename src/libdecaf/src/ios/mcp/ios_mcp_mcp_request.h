26d56cb847e716a98408b110b633d0db9553d2cb
James Benton <james.benton2@gmail.com>
2017-10-14 10:58:10 +0100

    ios/mcp: Add LoadLibraryChunk request.

--- src/libdecaf/src/ios/mcp/ios_mcp_mcp_request.h
+++ src/libdecaf/src/ios/mcp/ios_mcp_mcp_request.h
@@ -23,6 +23,22 @@ struct MCPRequestGetOwnTitleInfo
 CHECK_OFFSET(MCPRequestGetOwnTitleInfo, 0x00, unk0x00);
 CHECK_SIZE(MCPRequestGetOwnTitleInfo, 0x04);
 
+struct MCPRequestLoadLibraryChunk
+{
+   UNKNOWN(0x10);
+   be2_val<uint32_t> pos;
+   be2_val<MCPLibraryType> type;
+   be2_val<uint32_t> cafeProcessId;
+   UNKNOWN(0xC);
+   be2_array<char, 0x40> name;
+   UNKNOWN(0x12D8 - 0x68);
+};
+CHECK_OFFSET(MCPRequestLoadLibraryChunk, 0x10, pos);
+CHECK_OFFSET(MCPRequestLoadLibraryChunk, 0x14, type);
+CHECK_OFFSET(MCPRequestLoadLibraryChunk, 0x18, cafeProcessId);
+CHECK_OFFSET(MCPRequestLoadLibraryChunk, 0x28, name);
+CHECK_SIZE(MCPRequestLoadLibraryChunk, 0x12D8);
+
 struct MCPRequestSearchTitleList
 {
    be2_struct<MCPTitleListType> searchTitle;
