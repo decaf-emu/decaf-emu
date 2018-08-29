ca3ea69fde61cd5b899473789ad9b45eee07cbad
James Benton <james.benton2@gmail.com>
2018-08-29 12:14:42 +0100

    decaf-sdl: Add exports to use prioritise discrete Nvidia and AMD GPUs.

--- src/decaf-sdl/decafsdl_win.cpp
+++ src/decaf-sdl/decafsdl_win.cpp
@@ -7,6 +7,12 @@
 #include <SDL.h>
 #include <SDL_syswm.h>
 
+extern "C"
+{
+__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 void
 setWindowIcon(SDL_Window *window)
 {
