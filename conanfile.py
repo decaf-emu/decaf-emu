1fab30785d46d89413cb1c2d64c871cf21e1ee65
James Benton <james.benton2@gmail.com>
2018-12-15 14:43:00 +0000

    travis: Enable ffmpeg and Vulkan

--- conanfile.py
+++ conanfile.py
@@ -38,38 +38,50 @@ class DecafConan(ConanFile):
    generators = "cmake"
    settings = 'os'
    options = {
-      'accept_defaults': [True, False],
+      'silent': [True, False],
+      'ffmpeg': [True, False],
+      'curl': [True, False],
+      'openssl': [True, False],
+      'sdl2': [True, False],
+      'zlib': [True, False],
    }
    default_options = {
-      'accept_defaults': False,
+      'silent': False,
+      'ffmpeg': True,
+      'curl': True,
+      'openssl': True,
+      'sdl2': True,
+      'zlib': True,
+
       'ffmpeg:shared': True,
    }
 
    def requirements(self):
       dependency_list = [
-         ('ffmpeg/4.0.2@bincrafters/stable', 'yes'),
-         ('libcurl/7.61.1@bincrafters/stable', 'yes'),
-         ('OpenSSL/1.0.2n@conan/stable', 'yes'),
-         ('sdl2/2.0.9@bincrafters/stable', 'yes'),
-         ('zlib/1.2.11@conan/stable','yes')
+         ('ffmpeg/4.0.2@bincrafters/stable', 'yes' if self.options.ffmpeg else 'no'),
+         ('libcurl/7.61.1@bincrafters/stable', 'yes' if self.options.curl else 'no'),
+         ('OpenSSL/1.0.2n@conan/stable', 'yes' if self.options.openssl else 'no'),
+         ('sdl2/2.0.9@bincrafters/stable', 'yes' if self.options.sdl2 else 'no'),
+         ('zlib/1.2.11@conan/stable', 'yes' if self.options.zlib else 'no')
       ]
 
-      print 'Default enabled dependencies:'
-      for dependency, default in dependency_list:
-         if default == 'yes':
+      print 'Enabled dependencies:'
+      for dependency, enabled in dependency_list:
+         if enabled == 'yes':
             print "%s" % (dependency)
       print ''
 
-      print 'Default disabled dependencies:'
-      for dependency, default in dependency_list:
-         if default == 'no':
+      print 'Disabled dependencies:'
+      for dependency, enabled in dependency_list:
+         if enabled == 'no':
             print "%s" % (dependency)
       print ''
 
-      accept_defaults = self.options.accept_defaults
-      if not accept_defaults:
-         accept_defaults = not query_yes_no('Customise which dependencies to acquire from Conan?', default='no')
+      silent = self.options.silent
+      if not silent:
+         silent = not query_yes_no('Customise which dependencies to acquire from Conan?', default='no')
 
-      for dependency, default in dependency_list:
-         if accept_defaults or query_yes_no(dependency, default=default):
+      for dependency, enabled in dependency_list:
+         if (silent and enabled == 'yes') or \
+            (not silent and query_yes_no(dependency, default=enabled)):
             self.requires(dependency)
