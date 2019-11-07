adc758e01427d2654c30e9b68faf0da4e122d406
James Benton <james.benton2@gmail.com>
2019-11-07 22:45:30 +0000

    cafe/sysapp: Stub SYSGetStandardResult and SYSGetLauncherArgs.

--- src/libdecaf/src/cafe/libraries/sysapp/sysapp_callerargs.cpp
+++ src/libdecaf/src/cafe/libraries/sysapp/sysapp_callerargs.cpp
@@ -26,12 +26,32 @@ SYSGetCallerUPID()
    return 0u;
 }
 
+int32_t
+SYSGetLauncherArgs(virt_ptr<void> args)
+{
+   decaf_warn_stub();
+   std::memset(args.get(), 0, 0xC);
+   return 1;
+}
+
+int32_t
+SYSGetStandardResult(virt_ptr<uint32_t> arg1,
+                     uint32_t arg2,
+                     uint32_t arg3)
+{
+   decaf_warn_stub();
+   std::memset(arg1.get(), 0, 0x4);
+   return 1;
+}
+
 void
 Library::registerCallerArgsSymbols()
 {
    RegisterFunctionExport(SYSGetCallerPFID);
    RegisterFunctionExport(SYSGetCallerTitleId);
    RegisterFunctionExport(SYSGetCallerUPID);
+   RegisterFunctionExport(SYSGetStandardResult);
+   RegisterFunctionExportName("_SYSGetLauncherArgs", SYSGetLauncherArgs);
 }
 
 } // namespace cafe::sysapp
--- src/libdecaf/src/cafe/libraries/sysapp/sysapp_callerargs.h
+++ src/libdecaf/src/cafe/libraries/sysapp/sysapp_callerargs.h
@@ -13,4 +13,12 @@ SYSGetCallerTitleId();
 uint32_t
 SYSGetCallerUPID();
 
+int32_t
+SYSGetLauncherArgs(virt_ptr<void> args);
+
+int32_t
+SYSGetStandardResult(virt_ptr<uint32_t> arg1,
+                     uint32_t arg2,
+                     uint32_t arg3);
+
 } // namespace cafe::sysapp
