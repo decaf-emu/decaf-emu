27735c1aea12dc484f098174b0c26363d623e9fc
James Benton <james.benton2@gmail.com>
2019-12-08 22:59:10 +0000

    gpu/vulkan: Align shader push constant size to 16 bytes.

--- src/libgpu/src/spirv/spirv_pushconstants.h
+++ src/libgpu/src/spirv/spirv_pushconstants.h
@@ -14,7 +14,7 @@ struct Vec4
    float w;
 };
 
-struct VertexPushConstants
+struct alignas(16) VertexPushConstants
 {
    Vec4 posMulAdd;
    Vec4 zSpaceMul;
@@ -23,14 +23,22 @@ struct VertexPushConstants
 static constexpr int VertexPushConstantsSize = sizeof(VertexPushConstants);
 static constexpr int VertexPushConstantsOffset = 0;
 
-struct FragmentPushConstants
+struct alignas(16) FragmentPushConstants
 {
    uint32_t alphaFunc;
    float alphaRef;
    uint32_t needsPremultiply;
 };
 static constexpr int FragmentPushConstantsSize = sizeof(FragmentPushConstants);
-static constexpr int FragmentPushConstantsOffset = align_up<int>(VertexPushConstantsOffset + VertexPushConstantsSize, 16);
+static constexpr int FragmentPushConstantsOffset = VertexPushConstantsSize;
+
+// Vulkan only requires 4 byte alignment but it seems MoltenVK wants us to
+// align to 16 bytes.
+static_assert((VertexPushConstantsSize % 16) == 0);
+static_assert((VertexPushConstantsOffset % 16) == 0);
+
+static_assert((FragmentPushConstantsSize % 16) == 0);
+static_assert((FragmentPushConstantsOffset % 16) == 0);
 
 } // namespace spirv
 
